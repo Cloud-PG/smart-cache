d62bd955d83547455f849499274b9dee39367bef
MircoT <MircoT@users.noreply.github.com>
2018-12-19 13:27:26 +0100

    Fix reader and add json.gz basic writer

--- DataManager/collector/datafile/jsongz.py
+++ DataManager/collector/datafile/jsongz.py
@@ -1,8 +1,110 @@
 import gzip
 import json
+from string import whitespace
 
 
-__all__ = ['DataFileReader']
+__all__ = ['DataFileReader', 'DataFileWriter']
+
+
+class DataFileWriter(object):
+
+    """Write json.gz file."""
+
+    def __init__(self, filename, data=None):
+        """Init function of data writer for json.gz files.
+
+        Args:
+            filename (str): name of the json.gz file to write.
+            data (str, dict, list(str), list(dict)): initial data to be inserted
+
+        Returns:
+            DataFileWriter: the instance of this object
+
+        """
+        self.__filename = filename
+        self.__descriptor = gzip.open(self.__filename, "ab")
+        if data is not None:
+            self.append(data)
+
+    @staticmethod
+    def __valid_json(string):
+        """Check if a string is a valid JSON.
+
+        Args:
+            string (str): the string to analyse
+
+        Returns:
+            bool: if it is a valid JSON or not
+
+        """
+        try:
+            json.loads(string)
+        except ValueError:
+            return False
+        else:
+            return True
+
+    def __write(self, data):
+        """Write data to the json.gz file.
+
+        Args:
+            data (str): the JSON string to write
+
+        Returns:
+            DataFileWriter: this object instance
+
+        """
+        self.__descriptor.write(data.encode("utf-8") + b'\n')
+        return self
+
+    def append(self, data):
+        """Append data to the json.gz file.
+
+        Args:
+            data (str, dict, list(str), list(dict)): data to be inserted
+
+        Returns:
+            DataFileWriter: this object instance
+
+        """
+        if type(data) == str:
+            if self.__valid_json(data):
+                self.__write(data)
+        elif type(data) == dict:
+            self.__write(json.dumps(data))
+        elif type(data) == list:
+            if all(type(elm) == dict for elm in data):
+                for elm in data:
+                    self.__write(json.dumps(elm))
+            elif all(self.__valid_json(elm) for elm in data):
+                for elm in data:
+                    self.__write(elm)
+            else:
+                raise Exception(
+                    "You can pass only a list of 'dict' or JSON strings".format(type(data)))
+        else:
+            raise Exception(
+                "'{}' is not a valid input data type".format(type(data)))
+
+        return self
+
+    def __del__(self):
+        """Object destructor."""
+        if not self.__descriptor.closed:
+            self.__descriptor.close()
+
+    def __enter__(self):
+        """Initialization for 'with' statement.
+
+        Returns:
+            DataFileWriter: this object instance
+
+        """
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Closing function for the 'with' statement."""
+        self.__descriptor.close()
 
 
 class DataFileReader(object):
@@ -23,6 +125,7 @@ class DataFileReader(object):
         self.__descriptor = gzip.open(self.__filename, "rb")
         self.__last_index = -1
         self.__last_index_pos = None
+        self.__whitespaces = [elm.encode("utf-8") for elm in whitespace]
 
     def __get_json(self):
         """Extract a json object string from the file.
@@ -36,18 +139,22 @@ class DataFileReader(object):
         buffer = b''
         tmp_p = 0
         start = self.__descriptor.tell()
-        for cur_char in iter(lambda: self.__descriptor.read(1), ''):
+        for cur_char in iter(lambda: self.__descriptor.read(1), b''):
             buffer += cur_char
 
             if cur_char == b'{':
                 tmp_p += 1
             elif cur_char == b'}':
                 tmp_p -= 1
+            elif cur_char in self.__whitespaces and tmp_p == 0:
+                pass
 
             if tmp_p == 0 and len(buffer) >= 2:
                 json_obj_dict = json.loads(buffer, encoding="utf-8")
                 buffer = b''
                 return json_obj_dict, start
+
+        return (None, -1)
 
     @staticmethod
     def __gen_increasing_slice(slice):
@@ -140,7 +247,7 @@ class DataFileReader(object):
 
         """
         next_json, _ = self.__get_json()
-        if next_json:
+        if next_json is not None:
             return next_json
         else:
             raise StopIteration()
@@ -166,47 +273,19 @@ class DataFileReader(object):
 
 if __name__ == "__main__":
     ##
-    # Tests
+    # Test DataFileWriter
+    with DataFileWriter("test.json.gz", ['{"a": 2}']) as data:
+        data.append(json.dumps({}))
+        data.append([json.dumps({})])
+        data.append([{}, {"a": 2}, {}])
 
-    from sys import argv
+    ##
+    # Test DataFileReader
+    with DataFileReader("test.json.gz") as data:
+        for obj in data:
+            print(obj)
 
-    with DataFileReader(argv[1]) as data:
-        print(data[2])
-        print(data[2:0][0])
-        print("EQUAL?", data[2] == data[2:0][0])
-        print("EQUAL?", data[1] == data[2:0][1])
-        print("EQUAL?", data[10] == data[10:0][0])
-        print("===")
-        _10 = data[10]
-        __10 = data[10:0][0]
-        print(_10)
-        print(__10)
-        print(_10 == __10)
-        print("EQUAL?", data[10] == data[10:0][0])
-        data[0:2]
-        data[3]
+        print(data[0] == data[4])
 
-        data[10]
-        for data_ in data:
-            print(data_)
-            break
-
-        print("EQUAL?", data[0] == data[10:-1][-1])
-
-        print(data[0])
-        print("---")
-        data[1]
-        data[2]
-        data[1]
-        data[2]
-        print("---")
-        print("EQUAL?", data[10] == data[10:0][0])
-        print("---")
-        print(data[20])
-        print("---")
-        print(data[100])
-        print("---")
-
-    for data_ in DataFileReader(argv[1]):
+    for data_ in DataFileReader("test.json.gz"):
         print(data_)
-        break
