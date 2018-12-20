fc2ac0c46c49d1612a40e8812cfe8b5f98a3720d
MircoT <MircoT@users.noreply.github.com>
2018-12-20 11:36:26 +0100

    Fix base interface

--- DataManager/collector/interface/interface.py
+++ DataManager/collector/interface/interface.py
@@ -1,4 +1,5 @@
-from ..datafile.jsongz import DataFileReader as jsonGzReader
+from ..datafile.jsongz import JSONGzDataFileReader
+from ..datafile.avro import AvroDataFileReader
 from os import path
 
 
@@ -7,6 +8,7 @@ class DataInterface(object):
     def __init__(self, source):
         self.__source = source
         self.__data_collector = self.__get_collector(source)
+        self.__iter = None
         self.__index = 0
 
     @staticmethod
@@ -15,7 +17,9 @@ class DataInterface(object):
             filename, ext = path.splitext(source)
             if ext == ".gz":
                 if path.splitext(filename)[1] == ".json":
-                    return jsonGzReader(source)
+                    return JSONGzDataFileReader(source)
+            elif ext == ".avro":
+                return AvroDataFileReader(source)
 
         raise Exception(
             "Collector for source:\n  -> '{}'\nis not yet implemented...".format(source))
@@ -23,6 +27,9 @@ class DataInterface(object):
     def get_data(self):
         for data in self.__data_collector:
             yield data
+    
+    def __getitem__(self, idx):
+        return self.__data_collector[idx]
 
     def __iter__(self):
         """Initialize the DataInterface reader iterator.
@@ -31,7 +38,7 @@ class DataInterface(object):
             DataInterface: this object instance
 
         """
-        self.__index = 0
+        self.__iter = iter(self.__data_collector)
         return self
 
     def __next__(self):
@@ -44,9 +51,4 @@ class DataInterface(object):
             StopIteration: to end the iterator
 
         """
-        next_data = self.__data_collector[self.__index]
-        self.__index += 1
-        if next_data is not None:
-            return next_data
-        else:
-            raise StopIteration()
+        return next(self.__iter)
