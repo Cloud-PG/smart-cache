a7949d96a1c7083040d5dee1442de1909a60fee5
MircoT <MircoT@users.noreply.github.com>
2018-12-20 11:42:56 +0100

    Add avro datafile reader class

---
+++ DataManager/collector/datafile/avro.py
@@ -0,0 +1,110 @@
+from avro.io import DatumReader
+from avro.datafile import DataFileReader
+from . utils import gen_increasing_slice
+
+__all__ = ['DataFileReader']
+
+
+class AvroDataFileReader(object):
+
+    """Write avro file."""
+
+    def __init__(self, filename):
+        """Init function of data writer for json.gz files.
+
+        Args:
+            filename (str): name of the json.gz file to write.
+
+        Returns:
+            AvroDataFileReader: the instance of this object
+
+        """
+        self.__filename = filename
+        self.__descriptor = open(filename, 'rb')
+        self.__avro_file = None
+        self.__avro_iter = None
+
+    def __getitem__(self, idx):
+        """Select an item or a group of item from the file.
+
+        If the idx argument is a slice it is converted to a list
+        of indexes. All index lists will be processed in increasing
+        order (datafile.utils.gen_increasing_slice function for more details)
+        and then will be returned in the order requested by the user.
+
+        Args:
+            idx (int or slice): indexes to extract
+
+        Returns:
+            list or dict: a dictionary or a list of dictionary
+
+        """
+        self.__avro_file = DataFileReader(self.__descriptor, DatumReader())
+
+        if type(idx) is slice:
+            to_extract = [elm for elm in gen_increasing_slice(idx)]
+        else:
+            to_extract = [idx]
+
+        results = []
+        cur_idx = -1
+        self.__avro_iter = iter(self.__avro_file)
+
+        while len(to_extract):
+            try:
+                cur_elm = next(self.__avro_iter)
+            except StopIteration:
+                raise IndexError
+
+            cur_idx += 1
+
+            if cur_idx == to_extract[0]:
+                results.append(cur_elm)
+                to_extract.pop(0)
+
+        if type(idx) is slice:
+            if idx.start is not None and idx.stop is not None and idx.start > idx.stop:
+                return list(reversed(results))
+            return results
+        else:
+            return results.pop(0)
+
+    def __iter__(self):
+        """Initialize the JSONGz reader iterator.
+
+        Returns:
+            AvroDataFileReader: this object instance
+
+        """
+        self.__avro_file = DataFileReader(self.__descriptor, DatumReader())
+        self.__avro_iter = iter(self.__avro_file)
+        return self
+
+    def __next__(self):
+        """Get the next JSON object (inside iteration).
+
+        Returns:
+            dict: The JSON object converted in a dictionary
+
+        Raises:
+            StopIteration: to end the iterator
+
+        """
+        return next(self.__avro_iter)
+
+    def __del__(self):
+        """Object destructor."""
+        self.__avro_file.close()
+
+    def __enter__(self):
+        """Initialization for 'with' statement.
+
+        Returns:
+            AvroDataFileReader: this object instance
+
+        """
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """Closing function for the 'with' statement."""
+        self.__avro_file.close()
