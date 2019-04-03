61600c459c72ffefd3b5b010ed9f9ea148ca44af
MircoT <MircoT@users.noreply.github.com>
2019-04-03 18:12:00 +0200

    Add features and labels iterator

--- DataManager/collector/dataset/reader.py
+++ DataManager/collector/dataset/reader.py
@@ -18,6 +18,10 @@ class CMSDatasetV0Reader(object):
     def __init__(self, filename):
         self._collector = JSONDataFileReader(filename)
         self._meta = ReadableDictAsAttribute(self._collector[0])
+        self._use_feature_support = self.meta.support_tables.get(
+            'features', False)
+        self.__features = None
+        self.__feature_order = None
         self._collector.start_from(1)  # Skip metadata
         self._score_avg = None
 
@@ -25,12 +29,58 @@ class CMSDatasetV0Reader(object):
         return self.meta.len
 
     def __getitem__(self, index):
-        if isinstance(index, int):
-            return self._collector[index + 1]
-        elif isinstance(index, slice):
-            start, stop, stride = index
-            start += 1
-    
+        if self._use_feature_support:
+            if not self.__features:
+                self.__features = {}
+                self.__feature_order = set()
+                for name, table in self.meta.support_tables['features'].items():
+                    self.__features[name] = dict(
+                        (value, key)
+                        for key, value in table.items()
+                    )
+                    self.__feature_order |= set((name, ))
+                self.__feature_order = sorted(self.__feature_order)
+            res = self._collector[index]
+            if isinstance(res, list):
+                for idx, record in enumerate(res):
+                    res[idx] = np.array([
+                        float(
+                            self.__features[feature_name]
+                            [record['features']
+                             [feature_name]]
+                        )
+                        for feature_name in self.__feature_order]
+                    )
+                return np.array(res)
+            else:
+                return np.array([
+                    float(
+                        self.__features[feature_name]
+                        [res['features']
+                         [feature_name]]
+                    )
+                    for feature_name in self.__feature_order
+                ])
+        else:
+            return self._collector[index]
+
+    def features(self):
+        return self[:len(self)]
+
+    def labels(self, one_hot=True):
+        labels = []
+        for score in list(self.scores):
+            res = np.zeros((2,))
+            if one_hot:
+                res[int(score >= self.score_avg)] = 1
+                labels.append(res)
+            else:
+                labels.append(score >= self.score_avg)
+        return np.array(labels)
+
+    def toggle_feature_support(self):
+        self._use_feature_support = not self._use_feature_support
+
     @property
     def meta(self):
         return self._meta
@@ -50,11 +100,12 @@ class CMSDatasetV0Reader(object):
         if not self._score_avg:
             self._score_avg = sum(self.scores) / len(self)
         return self._score_avg
-    
+
     def scores_show(self):
         scores = list(self.scores)
         avg = self.score_avg
         plt.plot(range(len(scores)), scores, label="scores")
-        plt.plot(range(len(scores)), [avg for _ in range(len(scores))], label="avg")
+        plt.plot(range(len(scores)), [
+                 avg for _ in range(len(scores))], label="avg")
         plt.legend()
         plt.show()
