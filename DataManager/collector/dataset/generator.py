b5035545b8fff61d954918361cbd24793b289613
MircoT <MircoT@users.noreply.github.com>
2019-04-10 19:27:32 +0200

    Update generator.py

--- DataManager/collector/dataset/generator.py
+++ DataManager/collector/dataset/generator.py
@@ -219,7 +219,7 @@ class CMSDatasetV0(object):
 
     def spark_extract(self, start_date: str, window_size: int,
                       extract_support_tables: bool=True,
-                      num_partitions: int=100, chunk_size: int=5000,
+                      num_partitions: int=10, chunk_size: int=5000,
                       log_level: str="WARN"
                       ):
         """Extract data in a time window."""
