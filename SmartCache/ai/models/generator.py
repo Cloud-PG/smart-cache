e79ba6211e05f5f48851dd9e540c24a29f6eac56
MircoT <MircoT@users.noreply.github.com>
2019-04-15 16:45:39 +0200

    Add save model method

--- SmartCache/ai/models/generator.py
+++ SmartCache/ai/models/generator.py
@@ -31,3 +31,6 @@ class CMSSimpleRecordModelGenerator(object):
     def predict(self, data):
         predictions = self._model.predict(data)
         return np.argmax(predictions, axis=1)
+    
+    def save_model(self, out_name: str):
+        self._model.save("{}.h5".format(out_name))
