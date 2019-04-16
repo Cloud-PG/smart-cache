870bf59c0b0762a26a9e22be20d83de0a82124d7
MircoT <MircoT@users.noreply.github.com>
2019-04-16 16:42:08 +0200

    Add new evaluator

--- SmartCache/ai/models/evaluator.py
+++ SmartCache/ai/models/evaluator.py
@@ -3,14 +3,100 @@ import numpy as np
 from tqdm import tqdm
 
 
+class SimpleCache(object):
+
+    def __init__(self, init_state: dict={}):
+        self._cache = set()
+        self._hit = 0
+        self._miss = 0
+        self._history = []
+
+        if init_state:
+            self._cache |= init_state['cache']
+            self._hit += init_state['hit']
+            self._miss += init_state['miss']
+            self._history += init_state['history']
+
+    @property
+    def state(self):
+        return {
+            'cache': self._cache,
+            'hit': self._hit,
+            'miss': self._miss,
+            'history': self._history
+        }
+
+    @property
+    def history(self):
+        return list(zip(*self._history))
+
+    def hit_rate(self):
+        return float(self._hit / (self._hit + self._miss)) * 100.
+
+    def __len__(self):
+        return len(self._cache)
+
+    def check(self, file_):
+        return file_ in self._cache
+
+    def update(self, file_, insert: bool=True):
+        if not self.check(file_):
+            self._miss += 1
+            if insert:
+                self._cache |= set((file_, ))
+        else:
+            self._hit += 1
+        self._history.append(
+            len(self),
+            self.hit_rate()
+        )
+        return self
+
+
 class Evaluator(object):
 
-    def __init__(self, dataset, model):
+    def __init__(self, dataset, model, cache_type: str='simple', ai_cache_type: str='simple'):
         self._dataset = dataset
         self._model = model
+        self.__cache_type = cache_type
+        self.__ai_cache_type = ai_cache_type
+        self.__cache_types = {
+            'simple': SimpleCache
+        }
 
-    def _compare(self, initial_values: set=(), next_window: bool=False):
-        raise NotImplementedError
+    def _compare(
+        self, initial_values: dict={}, next_window: bool=False
+    ):
+        cache = self.__cache_types[self.__cache_type]()
+        ai_cache = self.__cache_types[self.__ai_cache_type]()
+
+        if initial_values:
+            cache = self.__cache_types[self.__cache_type](
+                initial_values['cache']
+            )
+            ai_cache = self.__cache_types[self.__ai_cache_type](
+                initial_values['ai_cache']
+            )
+
+        generator = None
+        if not next_window:
+            generator = self._dataset.get_raw_window()
+        else:
+            generator = self._dataset.get_raw_next_window()
+
+        for idx, obj in tqdm(enumerate(generator), desc="Simulation"):
+            FileName = obj['data']['FileName']
+            tensor = obj['tensor']
+
+            cache.update(FileName)
+
+            prediction = self._model.predict_single(tensor)
+            ai_cache.update(FileName, bool(prediction))
+
+        return {
+            'cache': cache,
+            'ai_cache': ai_cache
+        }
 
     def compare_window(self, show: bool=False):
         result = self._compare()
@@ -88,104 +174,5 @@ class Evaluator(object):
                  hit_rate['cache'], label="cache hit rate", alpha=0.9)
         plt.plot(range(len(hit_rate['ai_cache'])), hit_rate['ai_cache'],
                  label="ai_cache hit rate", alpha=0.9)
-        plt.xlabel("Num. request accepted x{}".format(stride))
+        plt.xlabel("Num. request accepted")
         plt.legend()
-
-
-class SimpleCacheFiniteSpaceLRU(Evaluator):
-
-    def __init__(self, dataset, model, max_num_files=1000):
-        super(SimpleCacheInfiniteSpace, self).__init__(dataset, model)
-        self.__cache_size = 1000
-
-    def _compare(
-        self, initial_values: set=(), next_window: bool=False
-    ):
-        pass
-
-
-class SimpleCacheInfiniteSpace(Evaluator):
-
-    def __init__(self, dataset, model):
-        super(SimpleCacheInfiniteSpace, self).__init__(dataset, model)
-
-    def _compare(
-        self, initial_values: dict={}, next_window: bool=False
-    ):
-        cache = set()
-        cache_hit_ratio = []
-        ai_cache = set()
-        ai_cache_hit_ratio = []
-
-        cache_hit = 0
-        cache_miss = 0
-        ai_cache_hit = 0
-        ai_cache_miss = 0
-
-        size_cache = []
-        size_ai_cache = []
-
-        if initial_values:
-            cache |= set(initial_values['cache']['files'])
-            cache_hit_ratio += initial_values['cache']['hit_ratio']
-            size_cache += initial_values['cache']['size']
-            ai_cache |= set(initial_values['ai_cache']['files'])
-            ai_cache_hit_ratio += initial_values['ai_cache']['hit_ratio']
-            size_ai_cache += initial_values['ai_cache']['size']
-            cache_hit = initial_values['cache']['hit']
-            cache_miss = initial_values['cache']['miss']
-            ai_cache_hit = initial_values['ai_cache']['hit']
-            ai_cache_miss = initial_values['ai_cache']['miss']
-
-        generator = None
-        if not next_window:
-            generator = self._dataset.get_raw_window()
-        else:
-            generator = self._dataset.get_raw_next_window()
-
-        for idx, obj in tqdm(enumerate(generator), desc="Simulation"):
-            FileName = obj['data']['FileName']
-            tensor = obj['tensor']
-
-            if FileName in cache:
-                cache_hit += 1
-            else:
-                cache_miss += 1
-                cache |= set((FileName,))
-
-            if FileName in ai_cache:
-                ai_cache_hit += 1
-            else:
-                ai_cache_miss += 1
-                prediction = self._model.predict_single(tensor)
-                if prediction != 0:
-                    ai_cache |= set((FileName,))
-
-            size_cache.append(len(cache))
-            size_ai_cache.append(len(ai_cache))
-
-            cache_hit_ratio.append(
-                float(cache_hit / (cache_hit + cache_miss)) * 100.
-                if cache_hit > 0 else 0.
-            )
-            ai_cache_hit_ratio.append(
-                float(ai_cache_hit / (ai_cache_hit + ai_cache_miss)) * 100.
-                if ai_cache_hit > 0 else 0.
-            )
-
-        return {
-            'cache': {
-                'files': cache,
-                'size': size_cache,
-                'hit_ratio': cache_hit_ratio,
-                'hit': cache_hit,
-                'miss': cache_miss
-            },
-            'ai_cache': {
-                'files': ai_cache,
-                'size': size_ai_cache,
-                'hit_ratio': ai_cache_hit_ratio,
-                'hit': ai_cache_hit,
-                'miss': ai_cache_miss
-            }
-        }
