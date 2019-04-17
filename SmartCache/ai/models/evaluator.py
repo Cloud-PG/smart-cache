0c2fe3e90fb0f6ff94cccbfdb012142c03fea692
MircoT <MircoT@users.noreply.github.com>
2019-04-17 16:14:23 +0200

    Add LRU and FIFO cache

--- SmartCache/ai/models/evaluator.py
+++ SmartCache/ai/models/evaluator.py
@@ -6,14 +6,14 @@ from tqdm import tqdm
 class SimpleCache(object):
 
     def __init__(self, init_state: dict={}):
-        self._cache = set()
+        self._cache = []
         self._hit = 0
         self._miss = 0
         self._size_history = []
         self._hit_rate_history = []
 
         if init_state:
-            self._cache |= init_state['cache']
+            self._cache += init_state['cache']
             self._hit += init_state['hit']
             self._miss += init_state['miss']
             self._size_history += init_state['size_history']
@@ -51,11 +51,14 @@ class SimpleCache(object):
     def check(self, file_):
         return file_ in self._cache
 
-    def update(self, file_, insert: bool=True):
+    def update(self, file_, insert: bool=True, insert_index: int=-1):
         if not self.check(file_):
             self._miss += 1
             if insert:
-                self._cache |= set((file_, ))
+                if insert_index == -1:
+                    self._cache.append(file_)
+                else:
+                    self._cache.insert(insert_index, file_)
         else:
             self._hit += 1
 
@@ -65,29 +68,95 @@ class SimpleCache(object):
         return self
 
 
+class FIFOCache(SimpleCache):
+
+    def __init__(self, *args, max_size: int=1000, **kwargs):
+        super(FIFOCache, self).__init__(*args, **kwargs)
+        self._max_size = max_size
+
+    def update(self, file_, insert: bool=True):
+        if len(self._cache) == self._max_size:
+            self._cache.pop(0)
+        super(FIFOCache, self).update(file_, insert)
+
+
+class LRUCache(SimpleCache):
+
+    def __init__(self, *args, max_size: int=1000, **kwargs):
+        super(LRUCache, self).__init__(*args, **kwargs)
+        self._max_size = max_size
+        self._counters = np.zeros(self._max_size)
+
+        if 'init_state' in kwargs:
+            self._max_size = kwargs['init_state']['max_size']
+            self._counters = np.array(kwargs['init_state']['counters'])
+
+    def update(self, file_, insert: bool=True):
+        self._counters += 1
+
+        if self.check(file_):
+            self._hit += 1
+            idx = self._cache.index(file_)
+            self._counters[idx] = 0
+        elif len(self._cache) == self._max_size:
+            self._miss += 1
+            if insert:
+                idx = np.argmin(self._counters)
+                self._cache.pop(idx)
+                self._cache.insert(idx, file_)
+                self._counters[idx] = 0
+        else:
+            self._miss += 1
+            if insert:
+                self._cache.append(file_)
+                idx = self._cache.index(file_)
+                self._counters[idx] = 0
+
+        self._size_history.append(len(self))
+        self._hit_rate_history.append(self.hit_rate)
+
+    @property
+    def state(self):
+        return {
+            'cache': self._cache,
+            'hit': self._hit,
+            'miss': self._miss,
+            'size_history': self._size_history,
+            'hit_rate_history': self._hit_rate_history,
+            'max_size': self._max_size,
+            'counters': list(self._counters)
+        }
+
+
 class Evaluator(object):
 
-    def __init__(self, dataset, model, cache_type: str='simple', ai_cache_type: str='simple'):
+    def __init__(self, dataset, model, cache_type: str='simple', ai_cache_type: str='simple', cache_settings: dict={}):
         self._dataset = dataset
         self._model = model
-        self.__cache_type = cache_type
-        self.__ai_cache_type = ai_cache_type
+        self.__cache_type = cache_type.lower()
+        self.__ai_cache_type = ai_cache_type.lower()
         self.__cache_types = {
-            'simple': SimpleCache
+            'simple': SimpleCache,
+            'lru': LRUCache,
+            'fifo': FIFOCache
         }
+        self.__cache_settings = cache_settings
 
     def _compare(
         self, initial_values: dict={}, next_window: bool=False
     ):
-        cache = self.__cache_types[self.__cache_type]()
-        ai_cache = self.__cache_types[self.__ai_cache_type]()
+        cache = self.__cache_types[self.__cache_type](**self.__cache_settings)
+        ai_cache = self.__cache_types[self.__ai_cache_type](
+            **self.__cache_settings)
 
         if initial_values:
             cache = self.__cache_types[self.__cache_type](
-                initial_values['cache'].state
+                initial_values['cache'].state,
+                **self.__cache_settings
             )
             ai_cache = self.__cache_types[self.__ai_cache_type](
-                initial_values['ai_cache'].state
+                initial_values['ai_cache'].state,
+                **self.__cache_settings
             )
 
         generator = None
@@ -183,9 +252,10 @@ class Evaluator(object):
         plt.plot(
             range(len(size['ai_cache'])),
             size['ai_cache'],
-            label="ai_cache size",
+            label="ai_cache [{}] hit rate".format(self.__ai_cache_type),
             alpha=0.9
         )
+        axes.set_ylabel("Num. stored files")
         axes.set_ylim(0)
         axes.set_xlim(0)
         plt.legend()
@@ -200,9 +270,10 @@ class Evaluator(object):
         plt.plot(
             range(len(hit_rate['ai_cache'])),
             hit_rate['ai_cache'],
-            label="ai_cache hit rate",
+            label="ai_cache [{}] hit rate".format(self.__ai_cache_type),
             alpha=0.9
         )
+        axes.set_ylabel("Hit rate %")
         axes.set_ylim(0, 100)
         axes.set_xlim(0)
         plt.xlabel("Num. request accepted")
