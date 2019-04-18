90a4493169d8394d7f2afc65ab5c7b59da56ab07
MircoT <MircoT@users.noreply.github.com>
2019-04-18 14:59:42 +0200

    Add load model and fix style

--- SmartCache/ai/models/generator.py
+++ SmartCache/ai/models/generator.py
@@ -16,20 +16,23 @@ class CMSSimpleRecordModelGenerator(object):
         self._model.compile(optimizer='adam',
                             loss='sparse_categorical_crossentropy',
                             metrics=['accuracy'])
-        self._epochs=epochs
+        self._epochs = epochs
 
     def train(self, dataset):
         train_data, train_labels = dataset.train_set(one_hot=False)
         self._model.fit(train_data, train_labels, epochs=self._epochs)
 
     def predict_single(self, data):
-        tmp = np.expand_dims(data,0)
+        tmp = np.expand_dims(data, 0)
         prediction = self._model.predict(tmp)
         return np.argmax(prediction[0])
-    
+
     def predict(self, data):
         predictions = self._model.predict(data)
         return np.argmax(predictions, axis=1)
-    
+
     def save(self, out_name: str):
         self._model.save("{}.h5".format(out_name))
+
+    def load(self, out_name: str):
+        self._model = keras.models.load_model("{}.h5".format(out_name))
