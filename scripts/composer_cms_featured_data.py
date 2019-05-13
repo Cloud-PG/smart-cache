384aa1fa75771033f44eca53971e805989518c85
MircoT <MircoT@users.noreply.github.com>
2019-05-13 16:09:54 +0200

    Update composer_cms_featured_data.py

--- scripts/composer_cms_featured_data.py
+++ scripts/composer_cms_featured_data.py
@@ -31,10 +31,10 @@ if __name__ == "__main__":
         ],
         source=cms_resource_manager,
         spark_conf={
-            'master': "local[8]",
+            'master': "local[6]",
             'config': {
                 'spark.driver.memory': "8g",
-                'spark.driver.maxResultSize': "4g",
+                'spark.driver.maxResultSize': "2g",
                 'spark.executor.memory': "2g"
             }
         }
