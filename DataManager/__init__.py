3f71bf3b0b073c7d83aed9b361b5545e0824a057
MircoT <MircoT@users.noreply.github.com>
2019-06-11 14:43:50 +0200

    Update DataManager init

---
+++ DataManager/__init__.py
@@ -0,0 +1,3 @@
+from .collector.api import DataFile
+from .collector.datafile.json import JSONDataFileReader, JSONDataFileWriter
+from .collector.datafile.avro import AvroDataFileReader, AvroDataFileWriter
