6d6426b431927b8889532d6d9ec1aa47913a22fa
MircoT <MircoT@users.noreply.github.com>
2019-06-19 16:39:20 +0200

    Update statistics.py

--- scripts/DataAnalysis/statistics.py
+++ scripts/DataAnalysis/statistics.py
@@ -4,6 +4,7 @@ import os
 from collections import OrderedDict
 from datetime import datetime, timedelta
 
+import matplotlib.pyplot as plt
 import urllib3
 from minio import Minio
 from minio.error import (BucketAlreadyExists, BucketAlreadyOwnedByYou,
@@ -60,6 +61,10 @@ class Statistics(object):
         self.__last_date = None
         self.__cur_date = None
 
+    @property
+    def data(self):
+        return self._data
+
     def add(self, date: tuple, filename: str):
         if self.__last_date != date:
             self.__cur_date = int(datetime(*date).timestamp())
@@ -67,7 +72,7 @@ class Statistics(object):
 
         if self.__cur_date not in self._data:
             self._data[self.__cur_date] = {
-                'num_records': 0,
+                'num_requests': 0,
                 'file_requests': OrderedDict()
             }
 
@@ -76,7 +81,7 @@ class Statistics(object):
         if filename not in cur_obj['file_requests']:
             cur_obj['file_requests'][filename] = 0
 
-        cur_obj['num_records'] += 1
+        cur_obj['num_requests'] += 1
         cur_obj['file_requests'][filename] += 1
 
     def to_dict(self):
@@ -85,11 +90,17 @@ class Statistics(object):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('start_date', type=str,
+    parser.add_argument('command', choices=['extract', 'plot'],
                         help="A string date: \"2019 5 5\"")
-    parser.add_argument('window_size', type=int,
+    parser.add_argument(['--stats-file', '-sf'], type=str,
+                        help="The json file to plot")
+    parser.add_argument(['--start-date', '-s'], type=str,
+                        help="A string date: \"2019 5 5\"")
+    parser.add_argument('--plot-dpi', type=int, default=300,
+                        help="DPI for plot output")
+    parser.add_argument(['--window-size', '-ws'], type=int,
                         help="Num. of days to extract")
-    parser.add_argument('--minio-config', type=str,
+    parser.add_argument(['--minio-config', '-mcfg'], type=str,
                         help='MinIO configuration in the form: "url key secret bucket"')
 
     args, _ = parser.parse_known_args()
@@ -97,31 +108,46 @@ def main():
 
     stats = Statistics()
 
-    if args.minio_config:
-        minio_client, bucket = create_minio_client(args.minio_config)
-        for year, month, day in period(args.start_date, args.window_size):
-            print(f"[Original Data][{year}-{month}-{day}]")
-            print("[Original Data][Download...]")
-            try:
-                minio_client.fget_object(
-                    f"{bucket}",
-                    f'year{year}_month{month}_day{day}.json.gz',
-                    './tmp.json.gz'
-                )
-            except ResponseError:
-                raise
-            print("[Original Data][Downloaded]")
-            print("[Original Data][Open File]")
-            collector = DataFile("./tmp.json.gz")
+    if args.command == "extract":
+        if args.minio_config:
+            minio_client, bucket = create_minio_client(args.minio_config)
+            for year, month, day in period(args.start_date, args.window_size):
+                print(f"[Original Data][{year}-{month}-{day}]")
+                print("[Original Data][Download...]")
+                try:
+                    minio_client.fget_object(
+                        f"{bucket}",
+                        f'year{year}_month{month}_day{day}.json.gz',
+                        './tmp.json.gz'
+                    )
+                except ResponseError:
+                    raise
+                print("[Original Data][Downloaded]")
+                print("[Original Data][Open File]")
+                collector = DataFile("./tmp.json.gz")
 
-            for record in tqdm(collector, desc=f"Extract statistics from {year}-{month}-{day}]"):
-                stats.add((year, month, day), record['FileName'])
+                for record in tqdm(collector, desc=f"Extract statistics from {year}-{month}-{day}]"):
+                    stats.add((year, month, day), record['FileName'])
 
-            os.remove("./tmp.json.gz")
+                os.remove("./tmp.json.gz")
 
-        with open("results.json", "w") as output_file:
-            json.dump(stats.to_dict(), output_file)
+            with open("results.json", "w") as output_file:
+                json.dump(stats.to_dict(), output_file)
+    elif args.command == 'plot':
+        if args.stats_file:
+            with open(args.stats_file) as stats_file:
+                result = json.load(stats_file)
 
+            plt.clf()
+            plt.hist([record['num_requests'] for record in result.values()])
+            plt.ylabel("Num. Requests")
+            plt.xlabel("Day")
+            plt.legend()
+            plt.tight_layout()
+            plt.savefig(f"{args.stats_file}.dayrequest.png", dpi=args.plot_dpi)
+
+        else:
+            raise Exception("You have to pass the '--stats-file' argument...")
     else:
         parser.print_usage()
 
