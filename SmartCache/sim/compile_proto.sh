a47398b09095afd37758dd6ecb2031619b486db0
MircoT <MircoT@users.noreply.github.com>
2019-08-19 13:11:03 +0200

    Update compile proto in python and fix lib sim in python

--- SmartCache/sim/compile_proto.sh
+++ SmartCache/sim/compile_proto.sh
@@ -6,3 +6,6 @@ python -m grpc_tools.protoc -I cache --python_out=. --grpc_python_out=. cache/si
 touch simService/__init__.py
 rm -rf pySimService
 mv -f simService pySimService
+sed -i -e 's/from\ simService/from\ \./g' ./pySimService/simService_pb2_grpc.py
+rm ./pySimService/simService_pb2_grpc.py-e
+
