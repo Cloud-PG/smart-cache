8a85c7be4cfe7d4eaa205a2b085090078e0663c2
MircoT <MircoT@users.noreply.github.com>
2019-08-22 13:48:14 +0200

    Remove init function from go interface for cache

--- SmartCache/sim/cache/base.go
+++ SmartCache/sim/cache/base.go
@@ -12,7 +12,6 @@ type Cache interface {
 	check(string) bool
 	updatePolicy(filename string, size float32, hit bool) bool
 
-	Init()
 	Clear()
 
 	HitRate() float32
@@ -24,6 +23,6 @@ type Cache interface {
 
 	SimServiceGet(context.Context, *pb.SimCommonFile) (*pb.SimCacheStatus, error)
 	SimServiceClear(context.Context, *empty.Empty) (*pb.SimCacheStatus, error)
-	SimServiceGetInfoCacheFiles(*empty.Empty, pb.SimService_SimServiceGetInfoCacheFilesServer) (error)
-	SimServiceGetInfoFilesWeights(*empty.Empty, pb.SimService_SimServiceGetInfoFilesWeightsServer) (error)
+	SimServiceGetInfoCacheFiles(*empty.Empty, pb.SimService_SimServiceGetInfoCacheFilesServer) error
+	SimServiceGetInfoFilesWeights(*empty.Empty, pb.SimService_SimServiceGetInfoFilesWeightsServer) error
 }
