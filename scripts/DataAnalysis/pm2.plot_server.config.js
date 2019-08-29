db70b1e23af2b554ba6514dc1e4befa539af5dff
MircoT <MircoT@users.noreply.github.com>
2019-08-29 17:21:06 +0200

    Fix typo

--- scripts/DataAnalysis/pm2.plot_server.config.js
+++ scripts/DataAnalysis/pm2.plot_server.config.js
@@ -1,71 +1,9 @@
 module.exports = {
   apps : [{
-    name: 'goCacheSim 10T',
-    script: 'goCacheSim',
-    interpreter: null,
+    name: 'Plot Server',
+    script: 'plot_server.py',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: "run weighted --port 5431 --size 10485760 --weightFunction FuncWeightedRequests",
-    instances: 1,
-    autorestart: true,
-    watch: true,
-    cwd: ".",
-  },
-  {
-    name: 'goCacheSim 100T',
-    script: 'goCacheSim',
-    interpreter: null,
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: "run weighted --port 5431 --size 104857600 --weightFunction FuncWeightedRequests",
-    instances: 1,
-    autorestart: true,
-    watch: true,
-    cwd: ".",
-  },
-  {
-    name: 'goCacheSim 200T',
-    script: 'goCacheSim',
-    interpreter: null,
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: "run weighted --port 5431 --size 209715200 --weightFunction FuncWeightedRequests",
-    instances: 1,
-    autorestart: true,
-    watch: true,
-    cwd: ".",
-  },
-  {
-    name: 'goCacheSim 10T LRU',
-    script: 'goCacheSim',
-    interpreter: null,
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: "run lru --port 5531 --size 10485760",
-    instances: 1,
-    autorestart: true,
-    watch: true,
-    cwd: ".",
-  },
-  {
-    name: 'goCacheSim 100T LRU',
-    script: 'goCacheSim',
-    interpreter: null,
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: "run lru --port 5531 --size 104857600",
-    instances: 1,
-    autorestart: true,
-    watch: true,
-    cwd: ".",
-  },
-  {
-    name: 'goCacheSim 200T LRU',
-    script: 'goCacheSim',
-    interpreter: null,
-
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: "run lru --port 5531 --size 209715200",
     instances: 1,
     autorestart: true,
     watch: true,
