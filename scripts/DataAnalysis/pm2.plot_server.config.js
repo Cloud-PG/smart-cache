8e70e17d9da1dbac5c3b8184b3171ae14a49c52b
MircoT <MircoT@users.noreply.github.com>
2019-08-29 17:51:43 +0200

    fix plot server

--- scripts/DataAnalysis/pm2.plot_server.config.js
+++ scripts/DataAnalysis/pm2.plot_server.config.js
@@ -6,7 +6,7 @@ module.exports = {
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     instances: 1,
     autorestart: true,
-    watch: true,
+    watch: false,
     cwd: ".",
   }],
 };
