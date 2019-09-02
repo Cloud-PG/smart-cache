46a2186ccfae8b8351d92c87a31bf72e3014fc2c
MircoT <MircoT@users.noreply.github.com>
2019-09-02 17:57:59 +0200

    Update weighted.go

--- SmartCache/sim/cache/weighted.go
+++ SmartCache/sim/cache/weighted.go
@@ -111,7 +111,7 @@ func fileWeightOnlyTime(totRequests float32, exp float32, lastTimeRequested time
 }
 
 func fileWeightedRequest(size float32, totRequests float32, meanTicks float32, exp float32) float32 {
-	return meanTicks + (((size / float32(math.Pow(float64(totRequests), float64(exp)))) / size) * 100.)
+	return meanTicks + (((size / totRequests) / size) * 100.)
 }
 
 // SimServiceGet updates the cache from a protobuf message
