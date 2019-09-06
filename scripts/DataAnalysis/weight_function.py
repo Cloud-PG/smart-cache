57ba12b8f4b4749f6bd06b1c3dcef8f547f73716
MircoT <MircoT@users.noreply.github.com>
2019-09-06 09:21:38 +0200

    Fix output ratio

--- scripts/DataAnalysis/weight_function.py
+++ scripts/DataAnalysis/weight_function.py
@@ -717,7 +717,10 @@ def simulate(cache, windows: list, region: str = "_all_",
                 desc_output += f"[Hit Rate {cur_hit_rate:06.2f}]"
                 desc_output += f"[Weighted Hit Rate {cur_weighted_hit_rate:06.2f}]"
                 desc_output += f"[Hit over miss {cur_hit_over_miss:06.2f}]"
-                desc_output += f"[Ratio {cur_read_on_hit/cur_written_data:0.2f}]"
+                try:
+                    desc_output += f"[Ratio {cur_read_on_hit/cur_written_data:0.2f}]"
+                except ZeroDivisionError:
+                    desc_output += f"[Ratio 0.00]"
                 desc_output += f"[Capacity {cur_capacity:06.2f}]"
 
                 record_pbar.desc = desc_output
