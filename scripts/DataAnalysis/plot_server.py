4952ddce7d69e543cf1f4f4f75b3c078795d8538
MircoT <MircoT@users.noreply.github.com>
2019-09-06 11:42:25 +0200

    Add all hits stats

--- scripts/DataAnalysis/plot_server.py
+++ scripts/DataAnalysis/plot_server.py
@@ -180,7 +180,7 @@ def plot_info_window(window: int, filename: str, **kwargs):
             pf_fileSize_hit_weighted_cache = figure(
                 title=f"Number of hits x file in {cache_name} (sorted by file size value, gray area)",
                 tools="box_zoom,pan,reset,save",
-                plot_width=kwargs.get('plot_width', 1280),
+                plot_width=kwargs.get('plot_width', 640),
                 plot_height=kwargs.get('plot_height', 200),
                 x_range=filenames_sort_by_size,
                 y_range=(1, int(max(cur_data['weights'].values())) + 10),
@@ -214,13 +214,135 @@ def plot_info_window(window: int, filename: str, **kwargs):
                     'y_axis_type', False) == 'log' else 0.0  # To avoid empty plot
             )
 
+            ##
+            # Number miss x file in weighted cache
+            pf_fileSize_miss_weighted_cache = figure(
+                title=f"Number of miss x file in {cache_name} (sorted by file size value, gray area)",
+                tools="box_zoom,pan,reset,save",
+                plot_width=kwargs.get('plot_width', 640),
+                plot_height=kwargs.get('plot_height', 200),
+                x_range=filenames_sort_by_size,
+                y_range=(1, int(max(cur_data['weights'].values())) + 10),
+                x_axis_type=None,
+                y_axis_type=kwargs.get('y_axis_type', 'auto'),
+            )
+
+            pf_fileSize_miss_weighted_cache.vbar(
+                filenames_sort_by_size,
+                top=[
+                    cur_data['stats'][filename]['size']
+                    for filename in filenames_sort_by_size
+                ],
+                color="gainsboro",
+                width=1.0,
+                bottom=0.01 if kwargs.get(
+                    'y_axis_type', False) == 'log' else 0.0  # To avoid empty plot
+            )
+
+            pf_fileSize_miss_weighted_cache.vbar(
+                filenames_sort_by_size,
+                top=[
+                    cur_data['stats'][filename]['nHits']
+                    if filename not in cur_data['cache']
+                    else 0
+                    for filename in filenames_sort_by_size
+                ],
+                color="blue",
+                width=1.0,
+                bottom=0.01 if kwargs.get(
+                    'y_axis_type', False) == 'log' else 0.0  # To avoid empty plot
+            )
+
+            ##
+            # Number hits x file in LRU cache
+            pf_fileSize_hit_LRU_cache = figure(
+                title=f"Number of hits x file in {cache_name} (sorted by file size value, gray area)",
+                tools="box_zoom,pan,reset,save",
+                plot_width=kwargs.get('plot_width', 640),
+                plot_height=kwargs.get('plot_height', 200),
+                x_range=filenames_sort_by_size,
+                y_range=(1, int(max(cur_data['weights'].values())) + 10),
+                x_axis_type=None,
+                y_axis_type=kwargs.get('y_axis_type', 'auto'),
+            )
+
+            pf_fileSize_hit_LRU_cache.vbar(
+                filenames_sort_by_size,
+                top=[
+                    cur_data['stats'][filename]['size']
+                    for filename in filenames_sort_by_size
+                ],
+                color="gainsboro",
+                width=1.0,
+                bottom=0.01 if kwargs.get(
+                    'y_axis_type', False) == 'log' else 0.0  # To avoid empty plot
+            )
+
+            pf_fileSize_hit_LRU_cache.vbar(
+                filenames_sort_by_size,
+                top=[
+                    caches['lru']['stats'][filename]['nHits']
+                    if filename in cur_data['cache']
+                    else 0
+                    for filename in filenames_sort_by_size
+                ],
+                color="red",
+                width=1.0,
+                bottom=0.01 if kwargs.get(
+                    'y_axis_type', False) == 'log' else 0.0  # To avoid empty plot
+            )
+
+            ##
+            # Number miss x file in LRU cache
+            pf_fileSize_miss_LRU_cache = figure(
+                title=f"Number of miss x file in {cache_name} (sorted by file size value, gray area)",
+                tools="box_zoom,pan,reset,save",
+                plot_width=kwargs.get('plot_width', 640),
+                plot_height=kwargs.get('plot_height', 200),
+                x_range=filenames_sort_by_size,
+                y_range=(1, int(max(cur_data['weights'].values())) + 10),
+                x_axis_type=None,
+                y_axis_type=kwargs.get('y_axis_type', 'auto'),
+            )
+
+            pf_fileSize_miss_LRU_cache.vbar(
+                filenames_sort_by_size,
+                top=[
+                    cur_data['stats'][filename]['size']
+                    for filename in filenames_sort_by_size
+                ],
+                color="gainsboro",
+                width=1.0,
+                bottom=0.01 if kwargs.get(
+                    'y_axis_type', False) == 'log' else 0.0  # To avoid empty plot
+            )
+
+            pf_fileSize_miss_LRU_cache.vbar(
+                filenames_sort_by_size,
+                top=[
+                    caches['lru']['stats'][filename]['nHits']
+                    if filename not in cur_data['cache']
+                    else 0
+                    for filename in filenames_sort_by_size
+                ],
+                color="red",
+                width=1.0,
+                bottom=0.01 if kwargs.get(
+                    'y_axis_type', False) == 'log' else 0.0  # To avoid empty plot
+            )
+
             figures.append(
                 column(
                     pf_fileW_hit_weighted_cache,
                     pf_fileW_hit_lru_cache,
                     row(
                         column(
-                            pf_fileSize_hit_weighted_cache
+                            pf_fileSize_hit_weighted_cache,
+                            pf_fileSize_hit_LRU_cache
+                        ),
+                        column(
+                            pf_fileSize_miss_weighted_cache,
+                            pf_fileSize_miss_LRU_cache
                         )
                     )
                 )
