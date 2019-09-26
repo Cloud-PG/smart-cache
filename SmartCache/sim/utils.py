c14132ec89692fac2399b2f50abaa66002b870cd
MircoT <MircoT@users.noreply.github.com>
2019-09-26 11:24:50 +0200

    Add get simulator method

---
+++ SmartCache/sim/utils.py
@@ -0,0 +1,13 @@
+from os import path
+import subprocess
+
+SIM_NAME = "goCacheSim"
+
+
+def get_simulator() -> str:
+    cur_dir = path.dirname(path.abspath(__file__))
+    sim_path = path.join(cur_dir, SIM_NAME)
+    if not path.exists(sim_path) or not path.isfile(sim_path):
+        subprocess.check_call(
+            f"go build -o {SIM_NAME} main.go", shell=True, cwd=cur_dir)
+    return sim_path
