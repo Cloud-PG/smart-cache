1042635feabb95f4e845b4872ca294cc9a6477ff
MircoT <MircoT@users.noreply.github.com>
2019-09-26 11:25:11 +0200

    Rename method get simulator

--- SmartCache/sim/utils.py
+++ SmartCache/sim/utils.py
@@ -4,7 +4,7 @@ import subprocess
 SIM_NAME = "goCacheSim"
 
 
-def get_simulator() -> str:
+def get_simulator_exe() -> str:
     cur_dir = path.dirname(path.abspath(__file__))
     sim_path = path.join(cur_dir, SIM_NAME)
     if not path.exists(sim_path) or not path.isfile(sim_path):
