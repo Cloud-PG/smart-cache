a4a79cc74874f1cbd0b35bb4cf3c1b95c159197f
MircoT <MircoT@users.noreply.github.com>
2019-09-27 15:14:25 +0200

    Fix basic tasks in simulation script

--- scripts/DataAnalysis/simulation.py
+++ scripts/DataAnalysis/simulation.py
@@ -18,6 +18,15 @@ CACHE_TYPES = {
 FORCE_CREATION = True
 
 
+def wait_jobs(processes):
+    while job_run(processes):
+        for process in processes:
+            try:
+                process.wait(timeout=0.5)
+            except subprocess.TimeoutExpired:
+                pass
+
+
 def read_output_last_line(output):
     buffer = ""
     cur_char = output.read(1).decode("ascii")
@@ -51,6 +60,45 @@ def main():
         path.abspath(__file__)), "simulation_results")
 
     processes = []
+
+    ##
+    # Single Window runs
+    single_window_run_dir = working_dir = path.join(
+        base_dir,
+        "run_single_window"
+    )
+    os.makedirs(single_window_run_dir, exist_ok=True)
+
+    for window_idx in range(START_FROM_WINDOW, STOP_WINDOW):
+        for cache_type in [cache for cache in CACHE_TYPES if cache != 'lru']:
+            working_dir = path.join(
+                single_window_run_dir,
+                f"{cache_type}_{CACHE_SIZE}_{REGION}",
+                f"{window_idx}",
+            )
+            os.makedirs(working_dir, exist_ok=True)
+            cur_process = subprocess.Popen(
+                " ".join([
+                    simulator_exe,
+                    "simulate",
+                    cache_type,
+                    path.abspath(RESULT_FOLDER),
+                    f"--size={CACHE_SIZE}",
+                    f"--simRegion={REGION}",
+                    f"--simWindowSize={WINDOW_SIZE}",
+                    f"--simStartFromWindow={window_idx}",
+                    f"--simStopWindow={window_idx+1}",
+                    "--simDump=true",
+                ]),
+                shell=True,
+                cwd=working_dir,
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+            processes.append(cur_process)
+
+    wait_jobs(processes)
 
     ##
     # Normal runs
@@ -87,48 +135,38 @@ def main():
         processes.append(cur_process)
     
     ##
-    # Single Window runs
-    single_window_run_dir = working_dir = path.join(
+    # Pair windows
+    normal_run_dir = working_dir = path.join(
         base_dir,
-        "run_single_window"
+        "run_full_normal"
     )
-    os.makedirs(single_window_run_dir, exist_ok=True)
-    
-    for window_idx in range(START_FROM_WINDOW, STOP_WINDOW):
-        for cache_type in [cache for cache in CACHE_TYPES if cache != 'lru']:
-            working_dir = path.join(
-                single_window_run_dir,
-                f"{cache_type}_{CACHE_SIZE}_{REGION}",
-                f"{window_idx}",
-            )
-            os.makedirs(working_dir, exist_ok=True)
-            cur_process = subprocess.Popen(
-                " ".join([
-                    simulator_exe,
-                    "simulate",
-                    cache_type,
-                    path.abspath(RESULT_FOLDER),
-                    f"--size={CACHE_SIZE}",
-                    f"--simRegion={REGION}",
-                    f"--simWindowSize={WINDOW_SIZE}",
-                    f"--simStartFromWindow={window_idx}",
-                    f"--simStopWindow={window_idx+1}",
-                    "--simDump=true",
-                ]),
-                shell=True,
-                cwd=working_dir,
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-            processes.append(cur_process)
+    os.makedirs(normal_run_dir, exist_ok=True)
 
-    while job_run(processes):
-        for process in processes:
-            try:
-                process.wait(timeout=0.5)
-            except subprocess.TimeoutExpired:
-                pass
+    for cache_type in CACHE_TYPES:
+        working_dir = path.join(
+            normal_run_dir,
+            f"{cache_type}_{CACHE_SIZE}_{REGION}"
+        )
+        os.makedirs(working_dir, exist_ok=True)
+        cur_process = subprocess.Popen(
+            " ".join([
+                simulator_exe,
+                "simulate",
+                cache_type,
+                path.abspath(RESULT_FOLDER),
+                f"--size={CACHE_SIZE}",
+                f"--simRegion={REGION}",
+                f"--simWindowSize={WINDOW_SIZE}",
+                f"--simStartFromWindow={START_FROM_WINDOW}",
+                f"--simStopWindow={STOP_WINDOW}",
+            ]),
+            shell=True,
+            cwd=working_dir,
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        processes.append(cur_process)
 
 
 if __name__ == "__main__":
