5ad7d5ce9108965ad845d80dda21c310a34b83b6
MircoT <MircoT@users.noreply.github.com>
2019-10-31 16:26:00 +0100

    Update generator.py

--- SmartCache/ai/models/generator.py
+++ SmartCache/ai/models/generator.py
@@ -40,9 +40,9 @@ class DonkeyModel(ai_pb2_grpc.AIServiceServicer):
             ])
         else:
             self._model = keras.Sequential([
-                keras.layers.Dense(512, activation='hard_sigmoid',
+                keras.layers.Dense(2048, activation='hard_sigmoid',
                                    input_shape=(input_size, )),
-                keras.layers.Dense(256, activation='hard_sigmoid'),
+                keras.layers.Dense(128, activation='hard_sigmoid'),
                 keras.layers.Dense(128, activation='hard_sigmoid'),
                 keras.layers.Dense(64, activation='hard_sigmoid'),
                 keras.layers.Dense(32, activation='hard_sigmoid'),
