d900dffd06244bbbcc703dee4af9b201fdb5cc0c
MircoT <MircoT@users.noreply.github.com>
2019-11-13 16:12:58 +0100

    Update __main__.py

--- SmartCache/sim/simulator/__main__.py
+++ SmartCache/sim/simulator/__main__.py
@@ -569,7 +569,7 @@ def main():
             )
 
             dataset_data = []
-            len_dataset = int(cur_df.shape[0] * 0.3)
+            len_dataset = int(cur_df.shape[0] * 0.2)
 
             for cur_row in tqdm(
                 cur_df.sample(n=len_dataset, random_state=42).itertuples(),
