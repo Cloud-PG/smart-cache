204ba7fc95cac2a066ae59833eaa6d661bd3f48b
MircoT <MircoT@users.noreply.github.com>
2019-11-27 11:00:40 +0100

    Add building anc compiling echoes

--- SmartCache/sim/build_sim.sh
+++ SmartCache/sim/build_sim.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+echo "[BUILD SIMULATOR]"
 go build -o bin -ldflags "-X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash=`git rev-parse HEAD`" ./...
+echo "[BUILD SIMULATOR][DONE]"
