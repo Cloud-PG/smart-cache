f1b6dad4eec802d3d599305f984f2c013ba7cc01
MircoT <MircoT@users.noreply.github.com>
2019-12-02 15:25:43 +0100

    Fix epsilon value

--- SmartCache/sim/cache/qLearn/qLearn.go
+++ SmartCache/sim/cache/qLearn/qLearn.go
@@ -97,7 +97,7 @@ func (table QTable) genAllStates(featureLenghts []int) chan []float64 {
 func (table *QTable) Init(featureLenghts []int) {
 	table.LearningRate = 0.9
 	table.DecayRate = 0.0 // No discount
-	table.DecayRateEpsilon = 0.000001
+	table.DecayRateEpsilon = 0.00001
 	table.Epsilon = 1.0
 	table.MaxEpsilon = 1.0
 	table.MinEpsilon = 0.1
