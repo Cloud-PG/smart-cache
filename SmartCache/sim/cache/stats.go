b7db2c2c6bfe4a2944b92131359b1f27d93181cb
MircoT <MircoT@users.noreply.github.com>
2020-01-14 13:02:35 +0100

    Change points formula

--- SmartCache/sim/cache/stats.go
+++ SmartCache/sim/cache/stats.go
@@ -183,7 +183,7 @@ func (stats *FileStats) updateFilePoints(curTime *time.Time) float64 {
 	numReq, numUsers, numSites := stats.getRealTimeStats(curTime)
 
 	dayDiffInCache := math.Floor(curTime.Sub(stats.InCacheSince).Hours() / 24.)
-	points := numReq*10 + numUsers*100. + numSites*1000. + float64(stats.Size)
+	points := numReq * 10. * numUsers * 100. * numSites * 1000. * float64(stats.Size)
 	points = points * math.Exp(-dayDiffInCache) // Decay points
 
 	stats.Points = points
