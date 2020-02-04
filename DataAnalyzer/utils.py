04786e32cd49318fa2e7978c7e3338bd4a732136
MircoT <MircoT@users.noreply.github.com>
2020-02-04 16:52:48 +0100

    Add delta time plots

--- DataAnalyzer/utils.py
+++ DataAnalyzer/utils.py
@@ -22,7 +22,7 @@ class FileStats(object):
         self.n_req = [0]
         self.n_users = [0]
         self.n_sites = [0]
-        self.delta_times = []
+        self.delta_times = [0]
         self.last_request = [start_delta_time]
         self.sizes = [start_size]
         self.users = set()
@@ -112,41 +112,45 @@ def plot_daily_stats(df: 'pd.DataFrame',
         #                   frequencies[key] for key in frequencie_keys]})
         # df.to_csv(f"week-{period:02d}.csv", index=False)
 
-    fig_n_req = figure(plot_width=1280, plot_height=240,
+    fig_n_req = figure(plot_width=1600, plot_height=320,
                        title="Num. Requests", x_axis_label="n-th request")
-    fig_n_users = figure(plot_width=1280, plot_height=240,
+    fig_n_users = figure(plot_width=1600, plot_height=320,
                          x_range=fig_n_req.x_range, title="Num. Users",
                          x_axis_label="n-th request")
-    fig_n_sites = figure(plot_width=1280, plot_height=240,
+    fig_n_sites = figure(plot_width=1600, plot_height=320,
                          x_range=fig_n_req.x_range, title="Num. Sites",
                          x_axis_label="n-th request")
-    fig_delta_times = figure(plot_width=1280, plot_height=240,
-                         x_range=fig_n_req.x_range, title="Delta times",
-                         x_axis_label="n-th request")
-    fig_sizes = figure(plot_width=1280, plot_height=240,
+    fig_delta_times = figure(plot_width=1600, plot_height=320,
+                             x_range=fig_n_req.x_range, title="Delta times",
+                             x_axis_label="n-th request")
+    fig_sizes = figure(plot_width=1600, plot_height=320,
                        x_range=fig_n_req.x_range, title="File sizes",
                        x_axis_label="n-th request",
                        y_axis_label="size (MB)")
-    fig_1req_sizes = figure(plot_width=1280, plot_height=240,
+    fig_1req_sizes = figure(plot_width=1600, plot_height=320,
                             x_range=fig_n_req.x_range, title="1 req. file sizes",
                             x_axis_label="n-th request",
                             y_axis_label="size (MB)")
-    fig_corr_numreqs_sizes = figure(plot_width=240, plot_height=240,
+    fig_corr_numreqs_sizes = figure(plot_width=320, plot_height=320,
                                     title="correlation num. reqs. and file sizes",
                                     x_axis_label="num. req.",
                                     y_axis_label="size (MB)")
-    fig_corr_numreqs_numusers = figure(plot_width=240, plot_height=240,
+    fig_corr_numreqs_numusers = figure(plot_width=320, plot_height=320,
                                        title="correlation num. reqs. and num. users",
                                        x_axis_label="num. req.",
                                        y_axis_label="num. users")
-    fig_corr_numreqs_numsites = figure(plot_width=240, plot_height=240,
+    fig_corr_numreqs_numsites = figure(plot_width=320, plot_height=320,
                                        title="correlation num. reqs. and num. sites",
                                        x_axis_label="num. req.",
                                        y_axis_label="num. sites")
-    fig_corr_meandelta_sizes = figure(plot_width=240, plot_height=240,
-                                       title="correlation mean delta times. and sizes",
-                                       x_axis_label="mean delta time",
-                                       y_axis_label="size (MB)")
+    fig_corr_numreqs_meandelta = figure(plot_width=320, plot_height=320,
+                                        title="correlation num. reqs. and mean delta times",
+                                        x_axis_label="num. req.",
+                                        y_axis_label="mean delta time")
+    fig_corr_meandelta_sizes = figure(plot_width=320, plot_height=320,
+                                      title="correlation mean delta times. and sizes",
+                                      x_axis_label="mean delta time",
+                                      y_axis_label="size (MB)")
 
     buff_xs = []
     buff_n_req = []
@@ -159,22 +163,29 @@ def plot_daily_stats(df: 'pd.DataFrame',
     buff_corr_numreqs_sizes = []
     buff_corr_numreqs_numusers = []
     buff_corr_numreqs_numsites = []
+    buff_corr_numreqs_meandelta = []
     buff_corr_meandelta_sizes = []
 
     for period, files in enumerate(stats, 1):
         for _, stats in tqdm(files.items(), desc=f"{_STATUS}Collect lines of period {period}"):
-            buff_xs += [stats.x]
-            buff_n_req.append(stats.n_req)
-            buff_n_users.append(stats.n_users)
-            buff_n_sites.append(stats.n_sites)
-            buff_delta_times.append(stats.delta_times)
-            buff_sizes.append(stats.sizes)
+            buff_xs += [stats.x[1:]]
+            buff_n_req.append(stats.n_req[1:])
+            buff_n_users.append(stats.n_users[1:])
+            buff_n_sites.append(stats.n_sites[1:])
+            buff_delta_times.append(stats.delta_times[1:])
+            buff_sizes.append(stats.sizes[1:])
             buff_corr_numreqs_sizes.append(
                 (stats.n_req[-1], stats.sizes[-1]))
             buff_corr_numreqs_numusers.append(
                 (stats.n_req[-1], stats.n_users[-1]))
             buff_corr_numreqs_numsites.append(
                 (stats.n_req[-1], stats.n_sites[-1]))
+            buff_corr_numreqs_meandelta.append(
+                (
+                    stats.n_req[-1],
+                    int(sum(stats.delta_times)/len(stats.delta_times)),
+                )
+            )
             buff_corr_meandelta_sizes.append(
                 (
                     int(sum(stats.delta_times)/len(stats.delta_times)),
@@ -184,8 +195,8 @@ def plot_daily_stats(df: 'pd.DataFrame',
 
     for period, files in enumerate(stats_1req, 1):
         for _, stats in tqdm(files.items(), desc=f"{_STATUS}Collect 1 req. file lines of period {period}"):
-            buff_1req_xs += [stats.x]
-            buff_1req_sizes.append(stats.sizes)
+            buff_1req_xs += [stats.x[1:]]
+            buff_1req_sizes.append(stats.sizes[1:])
             buff_corr_numreqs_sizes.append(
                 (stats.n_req[-1], stats.sizes[-1]))
             buff_corr_numreqs_numusers.append(
@@ -217,7 +228,7 @@ def plot_daily_stats(df: 'pd.DataFrame',
     print(f"{_STATUS}Plot delta times")
     fig_delta_times.scatter(
         [val for points in buff_xs for val in points],
-        [val for sizes in buff_delta_times for val in sizes],
+        [val for delta_times in buff_delta_times for val in delta_times],
         size=5
     )
     print(f"{_STATUS}Plot sizes")
@@ -247,11 +258,18 @@ def plot_daily_stats(df: 'pd.DataFrame',
         *zip(*buff_corr_numreqs_numsites),
         size=5
     )
+    print(f"{_STATUS}Plot correlation of num. reqs. and mean delta times")
+    fig_corr_numreqs_meandelta.scatter(
+        *zip(*buff_corr_numreqs_meandelta),
+        size=5
+    )
     print(f"{_STATUS}Plot correlation of mean delta times and sizes")
     fig_corr_meandelta_sizes.scatter(
         *zip(*buff_corr_meandelta_sizes),
         size=5
     )
+
+    fig_delta_times.yaxis.formatter = BasicTickFormatter(use_scientific=False)
 
     for fig in [fig_n_req, fig_n_users, fig_n_sites, fig_sizes, fig_1req_sizes]:
         fig.renderers.extend([
@@ -278,6 +296,7 @@ def plot_daily_stats(df: 'pd.DataFrame',
             fig_corr_numreqs_sizes,
             fig_corr_numreqs_numusers,
             fig_corr_numreqs_numsites,
+            fig_corr_numreqs_meandelta,
             fig_corr_meandelta_sizes
         ),
     )
