09486888a390f066208d97668dee00cfe0e5e5a2
Ubuntu <tedesk95@gmail.com>
2020-02-18 11:55:59 +0000

    add policy selector

--- deepQlearning/custom_gym/envs/cache/cache.py
+++ deepQlearning/custom_gym/envs/cache/cache.py
@@ -19,6 +19,16 @@ class FileStats(object):
         self._hit: int = 0
         self._miss: int = 0
         self._last_request: int = 0
+        self.recency: int = 0
+
+    def update_retrieve(self, size: float, hit: bool = False):
+        self._size = size
+        #if hit:
+        #    self._hit += 1
+        #else:
+        #    self._miss += 1
+
+        self.recency = 0
 
     def update(self, size: float, hit: bool = False):
         self._size = size
@@ -26,6 +36,8 @@ class FileStats(object):
             self._hit += 1
         else:
             self._miss += 1
+
+        self.recency = 0
 
     @property
     def tot_requests(self):
@@ -60,7 +72,7 @@ class Stats(object):
         return stats
 
 
-class LRU(object):
+class cache(object):
 
     def __init__(self, size: float = 104857600, h_watermark: float = 95., l_watermark: float = 75.):
         """Init of the cache object
@@ -72,9 +84,13 @@ class LRU(object):
         :param l_watermark: lower watermark (percentage), defaults to 75.
         :type l_watermark: float, optional
         """
+
         self._size: float = 0.0
         self._max_size = size
-        self._files = OrderedDict()
+
+        self._filesLRU = OrderedDict()
+        self._filesLFU = OrderedDict()
+        self._filesSize = OrderedDict()
 
         self._stats = Stats()
         # Stat attributes
@@ -105,30 +121,33 @@ class LRU(object):
         return 0.0
 
     def check(self, filename: str) -> bool:
-        return filename in self._files
+        return filename in self._filesLRU
 
     def before_request(self, filename, hit: bool, size, request: int) -> 'FileStats':
         stats = self._stats.get_or_set(filename, size, request)
         stats.update(size, hit)
         return stats
 
-    def update_policy(self, filename, file_stats, hit: bool, toadd: bool) -> bool:
-        if not hit and toadd == True:
-            #print(self._size + file_stats.size)
-            # print(self._max_size)
+    def before_request_retrieve(self, filename, hit: bool, size, request: int) -> 'FileStats':
+        stats = self._stats.get_or_set(filename, size, request)
+        stats.update_retrieve(size, hit)
+        return stats
+
+    def update_policy(self, filename, file_stats, hit: bool, action: int) -> bool:
+        if not hit and (action == 1 or action == 2 or action == 3 or action == 4):
             if self._size + file_stats.size <= self._max_size:
-                self._files[filename] = file_stats
+                self._filesLRU[filename] = file_stats
                 self._size += file_stats.size
                 return True
 
             else:
-                self.__free(file_stats.size)
-                self._files[filename] = file_stats
+                self.__free(file_stats.size, action)
+                self._filesLRU[filename] = file_stats
                 self._size += file_stats.size
                 return True
 
         elif hit:
-            self._files.move_to_end(filename)
+            self._filesLRU.move_to_end(filename)
 
         return False
 
@@ -145,27 +164,84 @@ class LRU(object):
 
         self._read_data += fileStats.size
 
-    def __free(self, amount: float, percentage: bool = False):
+    def __free(self, amount: float, action: int, percentage: bool = False):
         if not percentage:
             size_to_remove = amount
         else:
             size_to_remove = amount * (self._max_size / 100.)
         tot_removed = 0.0
+        #if action == 2:
+        #    filesLFU =  OrderedDict(sorted(self._filesLRU.items(), key=lambda t: t[1].tot_requests))
+        #if action == 3 or action == 4:
+        #    filesSize =  OrderedDict(sorted(self._filesLRU.items(), key=lambda t: t[1]._size))
+        print("I have to remove with action " + str(action))
+        #print(self._filesLRU)
         while tot_removed < size_to_remove:
-            _, file_stats = self._files.popitem(False)
+            if action == 1:                                                                         
+                _, file_stats = self._filesLRU.popitem(last = False)
+                #del self._filesLRU[_]
+                print("removed with action 1")
+            elif action == 2:
+                filesLFU =  OrderedDict(sorted(self._filesLRU.items(), key=lambda t: t[1].tot_requests))
+                _, file_stats = filesLFU.popitem(last = False)
+                del self._filesLRU[_]
+                print("removed with action 2")
+            elif action == 3:
+                filesSize =  OrderedDict(sorted(self._filesLRU.items(), key=lambda t: t[1]._size))
+                _, file_stats = filesSize.popitem(last = False)
+                del self._filesLRU[_]
+                print("removed with action 3")
+            elif action == 4:
+                filesSize =  OrderedDict(sorted(self._filesLRU.items(), key=lambda t: t[1]._size))
+                _, file_stats = filesSize.popitem(last = True)
+                del self._filesLRU[_]
+                print("removed with action 4")                
             tot_removed += file_stats.size
             self._size -= file_stats.size
             self._deleted_data += file_stats.size
 
-    def check_watermark_and_free(self):
+    def check_watermark_and_free(self, action):
         """Check if the cache reached the hight watermark.
 
         If it is true, the cache free space until
         the lower watermark is reached
         """
+        
         if self.capacity >= self._h_watermark:
             percentage = self.capacity - self._l_watermark
-            self.__free(percentage, percentage=True)
+            self.__free(amount = percentage, action = action)
+    
+    def update_recency(self):
+        for _, value in self._filesLRU.items():
+            value.recency += 1
+
+    def _get_mean_recency(self, curRequest):
+        #print(list(self._filesLRU))
+        if curRequest == 0:
+            return 0.
+        else:
+            list_=[]
+            for _,v in self._filesLRU.items():
+                list_.append(v.recency)
+            return np.array(list_).mean()
+    
+    def _get_mean_frequency(self, curRequest):
+        if curRequest == 0:
+            return 0.
+        else:
+            list_=[]
+            for _,v in self._filesLRU.items():
+                list_.append(v.tot_requests)
+            return np.array(list_).mean()
+    
+    def _get_mean_size(self, curRequest):
+        if curRequest == 0:
+            return 0.
+        else:
+            list_=[]
+            for _,v in self._filesLRU.items():
+                list_.append(v._size)
+            return np.array(list_).mean()
 
 
 ###############################################################################
@@ -205,12 +281,11 @@ def from_list_to_one_hot(list_):
 
     return one_hot_tot
 
-
 class CacheEnv(gym.Env):
 
     def write_stats(self):
         if self.curDay == self._idx_start:
-            with open('../dQl_100T_it_results_{}_startmonth{}_endmonth{}.csv'.format('onehot'+ str(self._one_hot),self._startMonth,self._endMonth), 'w', newline='') as file:
+            with open('../dQleviction_100T_it_results_{}_startmonth{}_endmonth{}.csv'.format('onehot'+ str(self._one_hot),self._startMonth,self._endMonth), 'w', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerow(
                     ['date',
@@ -228,45 +303,43 @@ class CacheEnv(gym.Env):
                      'CPU miss efficiency',
                      'cost'])
 
-        with open('../dQl_100T_it_results_{}_startmonth{}_endmonth{}.csv'.format('onehot'+ str(self._one_hot),self._startMonth,self._endMonth), 'a', newline='') as file:
+        with open('../dQleviction_100T_it_results_{}_startmonth{}_endmonth{}.csv'.format('onehot'+ str(self._one_hot),self._startMonth,self._endMonth), 'a', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(
                 [str(datetime.fromtimestamp(self.df.loc[0, 'reqDay']) + timedelta(days=1) ) + ' +0200 UTC',
-                 self._LRU._size,
-                 self._LRU.hit_rate() * 100.0,
-                 self._LRU._hit/self._LRU._miss * 100.0,
+                 self._cache._size,
+                 self._cache.hit_rate() * 100.0,
+                 self._cache._hit/self._cache._miss * 100.0,
                  0,
-                 self._LRU._written_data,
-                 self._LRU._read_data,
-                 self._LRU._dailyReadOnHit,
-                 self._LRU._dailyReadOnMiss,
-                 self._LRU._deleted_data,
-                 (self._LRU._CPUtime_hit + self._LRU._CPUtime_miss) /
-                    (self._LRU._WALLtime_hit +
-                     self._LRU._WALLtime_miss * 1.15) * 100.0,
-                    (self._LRU._CPUtime_hit)/(self._LRU._WALLtime_hit) * 100.0,
-                    (self._LRU._CPUtime_miss) /
-                 (self._LRU._WALLtime_miss * 1.15) * 100.0,
-                    self._LRU._written_data + self._LRU._read_data + self._LRU._deleted_data])
+                 self._cache._written_data,
+                 self._cache._read_data,
+                 self._cache._dailyReadOnHit,
+                 self._cache._dailyReadOnMiss,
+                 self._cache._deleted_data,
+                 (self._cache._CPUtime_hit + self._cache._CPUtime_miss) /
+                    (self._cache._WALLtime_hit +
+                     self._cache._WALLtime_miss * 1.15) * 100.0,
+                    (self._cache._CPUtime_hit)/(self._cache._WALLtime_hit) * 100.0,
+                    (self._cache._CPUtime_miss) /
+                 (self._cache._WALLtime_miss * 1.15) * 100.0,
+                    self._cache._written_data + self._cache._read_data + self._cache._deleted_data])
 
         return
 
     def reset_stats(self):
-        self._LRU._hit: int = 0
-        self._LRU._miss: int = 0
-        self._LRU._written_data: float = 0.0
-        self._LRU._deleted_data: float = 0.0
-        self._LRU._read_data: float = 0.0
+        self._cache._hit: int = 0
+        self._cache._miss: int = 0
+        self._cache._written_data: float = 0.0
+        self._cache._deleted_data: float = 0.0
+        self._cache._read_data: float = 0.0
 
-        self._LRU._dailyReadOnHit: float = 0.0
-        self._LRU._dailyReadOnMiss: float = 0.0
+        self._cache._dailyReadOnHit: float = 0.0
+        self._cache._dailyReadOnMiss: float = 0.0
 
-        self._LRU._CPUtime_hit: float = 0.0
-        self._LRU._WALLtime_hit: float = 0.0
-        self._LRU._CPUtime_miss: float = 0.0
-        self._LRU._WALLtime_miss: float = 0.0
-
-
+        self._cache._CPUtime_hit: float = 0.0
+        self._cache._WALLtime_hit: float = 0.0
+        self._cache._CPUtime_miss: float = 0.0
+        self._cache._WALLtime_miss: float = 0.0
 
         return
 
@@ -289,21 +362,26 @@ class CacheEnv(gym.Env):
         l.append(df_line['Size'])
         l.append(filestats.tot_requests)
         l.append(self.curRequest - filestats._last_request)
-        l.append(self._LRU._size/self._LRU._max_size)
+        l.append(self._cache._size/self._cache._max_size)
         datatype = (df_line['DataType'])
         if datatype == 'data':
             l.append(0.)
         else:
             l.append(1.)
+
+        l.append(self._cache._get_mean_recency(self.curRequest))
+        l.append(self._cache._get_mean_frequency(self.curRequest))
+        l.append(self._cache._get_mean_size(self.curRequest))
+        
+        #print(l)
         return np.asarray(l)
-        # return np.zeros(18)
 
     def get_one_hot(self, df_line, LRU, filestats):
         l = []
         l.append(df_line['Size'])
         l.append(filestats.tot_requests)
         l.append(self.curRequest - filestats._last_request)
-        l.append(self._LRU._size/self._LRU._max_size)
+        l.append(self._cache._size/self._cache._max_size)
         l.append(df_line['DataType'])
         return from_list_to_one_hot(l)
         # return np.zeros(18)
@@ -343,130 +421,155 @@ class CacheEnv(gym.Env):
         self.curDay = idx_start 
         self._totalDays = idx_end - idx_start
 
-        #self.df = df
-        #self.curTotalDailyRequest = len(df)
-        #self.reward_range = (-1, 1)
-
         # define action and observations spaces
-        self.action_space = gym.spaces.Discrete(2)
+        self.action_space = gym.spaces.Discrete(5)
         if self._one_hot == True:
             self.observation_space = gym.spaces.Box(
                 low=0, high=1, shape=(16,), dtype=np.float16)
         else:
             self.observation_space = gym.spaces.Box(
-                low=0, high=1, shape=(5,), dtype=np.float16)            
+                low=0, high=1, shape=(8,), dtype=np.float16)            
 
         print('Environment initialized')
 
     def step(self, action):
-
-        # get action from NN
+        
         if action == 0:
-            toadd = True
-        else:
+            #print('NOADD')
             toadd = False
+        elif action == 1:
+            #print('LRU')
+            toadd = True
+        elif action == 2:
+            #print('LFU')
+            toadd = True
+        elif action == 3:
+            #print('Size small')
+            toadd = True
+        elif action == 4:
+            #print('Size big')
+            toadd = True
+
+
+        #print(self._cache._filesLRU)
 
         # retrieve the updated stats before choice for this request
-        hit = self._LRU.check(self.df.loc[self.curRequest, 'Filename'])
+        hit = self._cache.check(self.df.loc[self.curRequest, 'Filename'])
         filename = self.df.loc[self.curRequest, 'Filename']
         size = self.df.loc[self.curRequest, 'Size']
-        filestats = self._LRU.before_request(
+        filestats = self._cache.before_request_retrieve(
             self.df.loc[self.curRequest, 'Filename'], hit, self.df.loc[self.curRequest, 'Size'], self.curRequest)
+        #filestats = self._cache._filesLRU[filename]
         cputime = self.df.loc[self.curRequest, 'CPUTime']
         walltime = self.df.loc[self.curRequest, 'WrapWC']
 
-        # modify cache and update stats according to the chosen action
-        added = self._LRU.update_policy(filename, filestats, hit, toadd)
-        self._LRU.after_request(filestats, hit, added)
-        self._LRU.check_watermark_and_free()
+        #print(filestats.tot_requests)
 
+        # modify cache and update stats according to the chosen action
+        added = self._cache.update_policy(filename, filestats, hit, action)
+
+        #print(filestats.tot_requests)
+        self._cache.after_request(filestats, hit, added)
+        self._cache.check_watermark_and_free(action)
+        #print(filestats.tot_requests)
         # compute the reward
         if hit == False:
-            self._LRU._WALLtime_miss += walltime
-            self._LRU._CPUtime_miss += cputime
+            self._cache._WALLtime_miss += walltime
+            self._cache._CPUtime_miss += cputime
             if toadd == True:
                 reward = 0
-                if self._LRU._dailyReadOnMiss >= bandwidthLimit:
+                if self._cache._dailyReadOnMiss >= bandwidthLimit:
                     reward -= float(size)
                 else:
                     reward += float(size)
             if toadd == False:
                 reward = 0
-                if self._LRU._dailyReadOnHit < self._LRU._dailyReadOnMiss/2.0 or self._LRU._dailyReadOnMiss > bandwidthLimit:
+                if self._cache._dailyReadOnHit < self._cache._dailyReadOnMiss/2.0 or self._cache._dailyReadOnMiss > bandwidthLimit:
                     reward -= float(size)
                 else:
                     reward += float(size)
         if hit == True:
-            self._LRU._WALLtime_hit += walltime
-            self._LRU._CPUtime_hit += cputime
+            self._cache._WALLtime_hit += walltime
+            self._cache._CPUtime_hit += cputime
             reward = 0.0
-            if self._LRU._dailyReadOnHit >= self._LRU._dailyReadOnMiss/2.0:
+            if self._cache._dailyReadOnHit >= self._cache._dailyReadOnMiss/2.0:
                 reward += float(size)
             else:
                 reward -= float(size)
 
         # get to next request and update stats
         self.curRequest += 1
+        self._cache.update_recency()
 
         # if the day is over, go to the next day, saving and resetting LRU stats
         done = False
         self.size_tot +=size
         print(str(self.curRequest) + ' / ' + str(self.df_length), end="\r")
-        #print(self.df_length)
 
         if (self.curRequest + 1) == self.df_length:
             self.write_stats()
             self.reset_stats()
-            #if (self.curDay+1) == self._totalDays:
-            #    done = True
-            #else:
             self.curDay += 1
             self.curRequest = 0
             self.get_dataframe(self.curDay)
 
         # update stats about the new request
         if done == False:
-            hit = self._LRU.check(self.df.loc[self.curRequest, 'Filename'])
-            filestats = self._LRU.before_request(
+            hit = self._cache.check(self.df.loc[self.curRequest, 'Filename'])
+            filestats = self._cache.before_request(
                 self.df.loc[self.curRequest, 'Filename'], hit, self.df.loc[0, 'Size'], self.curRequest)
 
         with open('reward.csv', 'a') as file:
             writer = csv.writer(file)
             writer.writerow([reward])
 
+        with open('eviction_policies.csv', 'a') as file:
+            writer = csv.writer(file)
+            if action == 0:
+                writer.writerow(['NOADD'])
+            elif action == 1:
+                writer.writerow(['LRU'])
+            elif action == 2:
+                writer.writerow(['LFU'])
+            elif action == 3:
+                writer.writerow(['Size small'])
+            elif action == 4:
+                writer.writerow(['Size big'])
+
         if self._one_hot == True:
-            return np.array(self.get_one_hot(self.df.loc[self.curRequest], self._LRU, filestats)), reward, done, {}
+            return np.array(self.get_one_hot(self.df.loc[self.curRequest], self._cache, filestats)), reward, done, {}
         else:
-            return np.array(self.get_simple_values(self.df.loc[self.curRequest], self._LRU, filestats)), reward, done, {}
+            return np.array(self.get_simple_values(self.df.loc[self.curRequest], self._cache, filestats)), reward, done, {}
+    
     def reset(self):
 
         with open('reward.csv', 'w') as file:
             writer = csv.writer(file)
             writer.writerow(['reward'])
 
+        with open('eviction_policies.csv', 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow(['eviction_policy'])
+
         # create cache
-        self._LRU = LRU()
+        self._cache = cache()
+
         self.size_tot=0
-        # begin with first request
+
         self.curRequest = 0
-        #self.curDay = 0
 
         self.get_dataframe(self.curDay)
 
-        hit = self._LRU.check(self.df.loc[self.curRequest, 'Filename'])
+        hit = self._cache.check(self.df.loc[self.curRequest, 'Filename'])
 
         # update stats before choice
-        filestats = self._LRU.before_request(
+        filestats = self._cache.before_request(
             self.df.loc[self.curRequest, 'Filename'], hit, self.df.loc[0, 'Size'], self.curRequest)
 
-        #print(self._startMonth)
-        #print(self._endMonth)
-        #print(self._totalDays)
-
         if self._one_hot == True:
-            return np.array(self.get_one_hot(self.df.loc[0], self._LRU, filestats))
+            return np.array(self.get_one_hot(self.df.loc[0], self._cache, filestats))
         
         else:
-            return np.array(self.get_simple_values(self.df.loc[0], self._LRU, filestats))
+            return np.array(self.get_simple_values(self.df.loc[0], self._cache, filestats))
         
 
--- deepQlearning/test_cache.py
+++ deepQlearning/test_cache.py
@@ -26,7 +26,7 @@ ENV_NAME = 'CacheEnv-v0'
 
 #env = gym.make(ENV_NAME,df = df_)
 
-env = gym.make(ENV_NAME, one_hot = True, start_month = 5, end_month = 12)
+env = gym.make(ENV_NAME, one_hot = False, start_month = 5, end_month = 10)
 
 nb_actions = env.action_space.n
 print(nb_actions)
