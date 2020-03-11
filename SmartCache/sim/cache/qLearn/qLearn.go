399396472e6df1246a4ac02865bf71404c920978
MircoT <MircoT@users.noreply.github.com>
2020-03-11 08:37:44 +0100

    Decrease decay rate

--- SmartCache/sim/cache/qLearn/qLearn.go
+++ SmartCache/sim/cache/qLearn/qLearn.go
@@ -76,7 +76,7 @@ func (table *QTable) Init(featureLenghts []int, role QTableRole, initEpsilon flo
 
 	table.LearningRate = 0.9 // also named Alpha
 	table.DiscountFactor = 0.5
-	table.DecayRateEpsilon = 0.000042
+	table.DecayRateEpsilon = 0.0000042
 	table.Epsilon = initEpsilon
 	table.MaxEpsilon = 1.0
 	table.MinEpsilon = 0.1
