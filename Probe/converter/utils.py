d1415ad2c45dbe217390e69c8de8b97c5d9eb72c
MircoT <MircoT@users.noreply.github.com>
2020-03-15 14:23:39 +0100

    Update utils.py

--- Probe/converter/utils.py
+++ Probe/converter/utils.py
@@ -65,8 +65,9 @@ def sort_from_avro(df: 'pd.DataFrame', cur_filename: str, order_folder: str) -> 
     df = df.set_index("Filename")
     ord_df = ord_df.set_index("Filename")
     # Reindex
-    new_df = df.reindex_like(ord_df, method=None).dropna()
-    df = new_df.reset_index()
+    new_index = df.reindex_like(ord_df, method=None).dropna()
+    df.set_index(new_index, inplace=True)
+    df.reset_index(inplace=True)
     # Remove duplicate counters
     df.Filename = df.Filename.apply(lambda elm: elm.rsplit("_#", 1)[
                                     0] if elm.find("_#") else elm)
