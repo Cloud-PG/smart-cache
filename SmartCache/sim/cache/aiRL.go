ec1f2cc5392e8543e7bd63b0eec2f50f1ef5c293
MircoT <MircoT@users.noreply.github.com>
2020-03-28 15:50:02 +0100

    Update aiRL.go

--- SmartCache/sim/cache/aiRL.go
+++ SmartCache/sim/cache/aiRL.go
@@ -386,7 +386,7 @@ func (cache *AIRL) UpdatePolicy(request *Request, fileStats *FileStats, hit bool
 				// 	reward = -reward
 				// }
 
-				if !hit || cache.qEvictionPrevState.HitRate > cache.HitRate() || cache.dataReadOnHit <= cache.dataReadOnMiss || cache.dataReadOnMiss >= (cache.bandwidth*0.95) {
+				if !hit || cache.qEvictionPrevState.HitRate > cache.HitRate() || cache.dataReadOnHit <= cache.dataReadOnMiss || cache.dataReadOnMiss >= (cache.bandwidth*0.5) {
 					reward = -reward
 				}
 				// Update table
@@ -521,7 +521,7 @@ func (cache *AIRL) UpdatePolicy(request *Request, fileStats *FileStats, hit bool
 					// if cache.dailyReadOnHit < cache.dailyReadOnMisss*2.) || cache.dataReadOnHit < (cache.dataReadOnMiss*2.) {
 					// 	reward = -reward
 					// }
-					if fileStats.Frequency > 1 && (cache.dataReadOnHit <= cache.dataReadOnMiss || cache.dataReadOnMiss <= (cache.bandwidth*0.95)) {
+					if fileStats.Frequency > 1 && (cache.dataReadOnHit <= cache.dataReadOnMiss || cache.dataReadOnMiss <= (cache.bandwidth*0.5)) {
 						reward = -reward
 					}
 
@@ -579,7 +579,7 @@ func (cache *AIRL) UpdatePolicy(request *Request, fileStats *FileStats, hit bool
 					// if cache.dataReadOnHit < (cache.dataReadOnMiss*2.) || cache.dailyReadOnHit < cache.dailyReadOnMisss*2.) {
 					// 	reward = -reward
 					// }
-					if fileStats.Frequency == 1 && (cache.dataWritten >= (cache.dataReadOnHit*0.3) || cache.dataReadOnMiss >= (cache.bandwidth*0.95)) {
+					if fileStats.Frequency == 1 && (cache.dataWritten >= (cache.dataReadOnHit*0.3) || cache.dataReadOnMiss >= (cache.bandwidth*0.5)) {
 						reward = -reward
 					}
 					// Update table
@@ -620,7 +620,7 @@ func (cache *AIRL) UpdatePolicy(request *Request, fileStats *FileStats, hit bool
 					// if cache.dataReadOnHit < (cache.dataReadOnMiss*2.) || cache.dailyReadOnHit < cache.dailyReadOnMisss*2.) {
 					// 	reward = -reward
 					// }
-					if cache.qEvictionPrevState.HitRate > cache.HitRate() || cache.dataReadOnHit <= cache.dataReadOnMiss || cache.dataReadOnMiss >= (cache.bandwidth*0.95) {
+					if cache.qEvictionPrevState.HitRate > cache.HitRate() || cache.dataReadOnHit <= cache.dataReadOnMiss || cache.dataReadOnMiss >= (cache.bandwidth*0.5) {
 						reward = -reward
 					}
 
