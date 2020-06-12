5e88fc69167cea7997d24f2d7347dff8d85891de
MircoT <MircoT@users.noreply.github.com>
2020-06-12 12:00:15 +0200

    remove unuseful requirements

--- setup.py
+++ setup.py
@@ -1,36 +1,21 @@
-from distutils.core import setup
+from distutils.extension import Extension
+
+import numpy
+from Cython.Build import cythonize
+from setuptools import setup
+
+extensions = [
+    Extension(
+        "cacheenvnewcython",
+        sources=["cacheenvnewcython.pyx"],
+        include_dirs=[
+            numpy.get_include()
+        ],
+        extra_compile_args=["-O3"],
+        language="c++"
+    )
+]
 
 setup(
-    name='SmartCacheTools',
-    version='0.0.0',
-    author='Mirco Tracolli',
-    author_email='mirco.tracolli@pg.infn.it',
-    packages=[
-        'Probe',
-        'Probe.analyzer',
-        'Probe.plotter',
-        'Probe.converter',
-        'Probe.qTable',
-        'DataManager',
-        'DataManager.agent',
-        'DataManager.agent.converter',
-        'DataManager.collector',
-        'DataManager.collector.datafeatures',
-        'DataManager.collector.datafile',
-        'DataManager.collector.dataset',
-        'SmartCache',
-        'SmartCache.ai',
-        'SmartCache.ai.models'
-    ],
-    scripts=[],
-    url='https://github.com/Cloud-PG/smart-cache',
-    license='Apache 2.0 License',
-    description='Tool collection for SmartCache.',
-    long_description="To do...",
-    install_requires=open("requirements.txt").read(),
-    classifier=[
-        "Operating System :: POSIX :: Linux",
-        "License :: OSI Approved :: Apache 2.0 License",
-        "Programming Language :: Python :: 3 :: Only"
-    ]
+    ext_modules=cythonize(extensions)
 )
