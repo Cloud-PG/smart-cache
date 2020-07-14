7ec41e80f54598add35fc03b15ef960b448c5823
MircoT <MircoT@users.noreply.github.com>
2020-07-14 13:02:33 +0200

    Change decay rate on italian region

--- scripts/simulations/sim_italian_dataset_half_size.sh
+++ scripts/simulations/sim_italian_dataset_half_size.sh
@@ -3,7 +3,7 @@
 python -m SmartCache.sim.simulator sim ../../datasets/source2018_numeric_it_with_avro_order --force-exe-build 'true' --cache-type lru,lfu,sizeSmall,sizeBig --cache-size 50 --cache-size-unit "T" --simulation-steps normal --window-start 0 --window-stop 52 --region it --cache-bandwidth 10 --cache-bandwidth-redirect 'true' --out-folder ../../results/sim_italian_dataset/cache_50T_10Gbit &&
 
 # ------------------------ RL italian dataset  ----------------------------
-python -m SmartCache.sim.simulator simAI ../../datasets/source2018_numeric_it_with_avro_order --force-exe-build 'true' --simulation-steps normal --window-start 0 --window-stop 52 --cache-type aiRL --region it --cache-size 50 --cache-size-unit "T" --cache-bandwidth 10 --cache-bandwidth-redirect 'true' --out-folder ../../results/sim_italian_dataset/cache_50T_10Gbit --ai-rl-addition-feature-map ../../featureMaps/rlAdditionFeatureMap.json --ai-rl-eviction-feature-map ../../featureMaps/rlEvictionFeatureMap.json --load-prev-normal-run 'false' --dump-files-and-stats 'false' --decay-rate-epsilon 0.000005 &&
+python -m SmartCache.sim.simulator simAI ../../datasets/source2018_numeric_it_with_avro_order --force-exe-build 'true' --simulation-steps normal --window-start 0 --window-stop 52 --cache-type aiRL --region it --cache-size 50 --cache-size-unit "T" --cache-bandwidth 10 --cache-bandwidth-redirect 'true' --out-folder ../../results/sim_italian_dataset/cache_50T_10Gbit --ai-rl-addition-feature-map ../../featureMaps/rlAdditionFeatureMap.json --ai-rl-eviction-feature-map ../../featureMaps/rlEvictionFeatureMap.json --load-prev-normal-run 'false' --dump-files-and-stats 'false' --decay-rate-epsilon 0.000001 &&
 
 ### ----- Plot results -----
 
