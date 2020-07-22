a0dd22859f6430a20ff9ab6de450b2a11bcad8c7
MircoT <MircoT@users.noreply.github.com>
2020-07-22 12:18:55 +0200

    Change negative colors

--- SmartCache/sim/simulator/plotter.py
+++ SmartCache/sim/simulator/plotter.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 from bokeh.models import (BasicTickFormatter, FuncTickFormatter, Legend,
                           Range1d, SaveTool, Span)
-from bokeh.palettes import Category20, Blues, OrRd
+from bokeh.palettes import Category20, Blues, Oranges
 from bokeh.plotting import figure
 
 from .utils import get_cache_size
@@ -462,7 +462,7 @@ def plot_measure(tools: list,
                 if "Action store" not in values.columns:
                     continue
                 not_colors = cycle(Blues[9])
-                negative_colors = cycle(OrRd[9])
+                negative_colors = cycle(Oranges[9])
                 for action in [column
                                for column in values.columns
                                if column.lower().find("store") != -1 and
@@ -487,7 +487,7 @@ def plot_measure(tools: list,
                 if "Action delete" not in values.columns:
                     continue
                 not_colors = cycle(Blues[9])
-                negative_colors = cycle(OrRd[9])
+                negative_colors = cycle(Oranges[9])
                 for action in [column
                                for column in values.columns
                                if column.lower().find(" delete") != -1 and
