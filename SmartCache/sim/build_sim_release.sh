c8392fda75b31fb7e06dfd295ea36092656f7b7d
MircoT <MircoT@users.noreply.github.com>
2020-08-01 12:44:55 +0200

    Update build scripts

--- SmartCache/sim/build_sim_release.sh
+++ SmartCache/sim/build_sim_release.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 echo "[BUILD SIMULATOR]" &&
-go build -o bin -ldflags "-s -w -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash=`git rev-parse HEAD`" ./... &&
+go build -a -v -o bin -ldflags "-s -w -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash=`git rev-parse HEAD`" ./... &&
+# upx --brute bin/simulator # to cut executable size by compression
 echo "[BUILD SIMULATOR][DONE]"
