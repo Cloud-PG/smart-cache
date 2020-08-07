64df530ef7fdb962197daf67ab9ad54d36ea77cb
MircoT <MircoT@users.noreply.github.com>
2020-08-07 12:01:20 +0200

    Fix only eviction script output folders

--- scripts/simulations/sim_italian_dataset_half_size_only_eviction.sh
+++ scripts/simulations/sim_italian_dataset_half_size_only_eviction.sh
@@ -2,19 +2,19 @@
 
 python -m SmartCache.sim.utils compile --release "true" &&
 
-simulator sim ../../config/simulations/italian_dataset_half_size_only_eviction/100T/lru.yml &&
-simulator sim ../../config/simulations/italian_dataset_half_size_only_eviction/100T/lfu.yml &&
-simulator sim ../../config/simulations/italian_dataset_half_size_only_eviction/100T/sizeBig.yml &&
-simulator sim ../../config/simulations/italian_dataset_half_size_only_eviction/100T/sizeSmall.yml &&
+simulator sim ../../config/simulations/italian_dataset_only_eviction/50T/lru.yml &&
+simulator sim ../../config/simulations/italian_dataset_only_eviction/50T/lfu.yml &&
+simulator sim ../../config/simulations/italian_dataset_only_eviction/50T/sizeBig.yml &&
+simulator sim ../../config/simulations/italian_dataset_only_eviction/50T/sizeSmall.yml &&
 
-simulator sim ../../config/simulations/italian_dataset_half_size_only_eviction/100T/scdl.yml &&
+simulator sim ../../config/simulations/italian_dataset_only_eviction/50T/scdl.yml &&
 
 ### ----- Plot results -----
 
-python -m SmartCache.sim.simulator plot ../../results/sim_italian_dataset_half_size_only_eviction/100T --plot-resolution 1280,800 --cache-bandwidth 10 --export-table 'true'
+python -m SmartCache.sim.simulator plot ../../results/sim_italian_dataset_only_eviction/50T --plot-resolution 1280,800 --cache-bandwidth 10 --export-table 'true'
 
 ### ----- Plot tables -----
 
-# python -m Probe.qTable ../../results/sim_italian_dataset_half_size_only_eviction/100T/run_full_normal/aiRL_10G_it/aiRL_10G_it_additionQtable.csv &&
+# python -m Probe.qTable ../../results/sim_italian_dataset_only_eviction/50T/run_full_normal/aiRL_10G_it/aiRL_10G_it_additionQtable.csv &&
 
-# python -m Probe.qTable ../../results/sim_italian_dataset_half_size_only_eviction/100T/run_full_normal/aiRL_10G_it/aiRL_10G_it_evictionQtable.csv &
+# python -m Probe.qTable ../../results/sim_italian_dataset_only_eviction/50T/run_full_normal/aiRL_10G_it/aiRL_10G_it_evictionQtable.csv &
