4615e38c3ee1ac1f90180980913d9de0ae0e94f1
MircoT <MircoT@users.noreply.github.com>
2020-08-11 20:10:15 +0200

    fix script

--- scripts/simulations/sim_italian_dataset_half_size_only_eviction_no_k.sh
+++ scripts/simulations/sim_italian_dataset_half_size_only_eviction_no_k.sh
@@ -11,10 +11,10 @@ simulator sim ../../config/simulations/italian_dataset_no_k_only_eviction/50T/sc
 
 ### ----- Plot results -----
 
-python -m SmartCache.sim.simulator plot ../../results/sim_italian_dataset_only_eviction_no_k/50T --plot-resolution 1280,800 --cache-bandwidth 10 --export-table 'true'
+python -m SmartCache.sim.simulator plot ../../results/sim_italian_dataset_no_k_only_eviction/50T --plot-resolution 1280,800 --cache-bandwidth 10 --export-table 'true'
 
 ### ----- Plot tables -----
 
-# python -m Probe.qTable ../../results/sim_italian_dataset_only_eviction_no_k/50T/run_full_normal/aiRL_10G_it/aiRL_10G_it_additionQtable.csv &&
+# python -m Probe.qTable ../../results/sim_italian_dataset_no_k_only_eviction/50T/run_full_normal/aiRL_10G_it/aiRL_10G_it_additionQtable.csv &&
 
-# python -m Probe.qTable ../../results/sim_italian_dataset_only_eviction_no_k/50T/run_full_normal/aiRL_10G_it/aiRL_10G_it_evictionQtable.csv &
+# python -m Probe.qTable ../../results/sim_italian_dataset_no_k_only_eviction/50T/run_full_normal/aiRL_10G_it/aiRL_10G_it_evictionQtable.csv &
