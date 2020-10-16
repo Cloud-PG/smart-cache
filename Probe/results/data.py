c232bc3d039cc6035d69b471408e723fd7f9a15c
MircoT <MircoT@users.noreply.github.com>
2020-10-16 12:16:21 +0200

    Add extended table toggle

--- Probe/results/data.py
+++ Probe/results/data.py
@@ -1,6 +1,7 @@
 import json
 import pathlib
 import re
+from typing import Tuple
 
 import pandas as pd
 from tqdm import tqdm
@@ -8,73 +9,69 @@ from tqdm import tqdm
 from ..utils import STATUS_ARROW
 from .utils import parse_sim_log
 
-_ALGORITHMS = ['lru', 'lfu', 'sizeSmall', 'sizeBig']
+_ALGORITHMS = ["lru", "lfu", "sizeSmall", "sizeBig"]
 
 SIM_RESULT_FILENAME = "simulation_results.csv"
 SIM_CHOICE_LOG_FILE = "simulationLogFile.csv.gz"
 
 COLUMNS = [
-    'date',
-    'num req',
-    'num hit',
-    'num added',
-    'num deleted',
-    'num redirected',
-    'size redirected',
-    'cache size',
-    'num miss after delete',
-    'size',
-    'capacity',
-    'bandwidth',
-    'bandwidth usage',
-    'hit rate',
-    'weighted hit rate',
-    'written data',
-    'read data',
-    'read on hit data',
-    'read on miss data',
-    'deleted data',
-    'avg free space',
-    'std dev free space',
-    'CPU efficiency',
-    'CPU hit efficiency',
-    'CPU miss efficiency',
-    'CPU efficiency upper bound',
-    'CPU efficiency lower bound',
-    'Addition epsilon',
-    'Eviction epsilon',
-    'Addition qvalue function',
-    'Eviction qvalue function',
-    'Eviction calls',
-    'Eviction forced calls',
-    'Eviction mean num categories',
-    'Eviction std dev num categories',
-    'Action store',
-    'Action not store',
-    'Action delete all',
-    'Action delete half',
-    'Action delete quarter',
-    'Action delete one',
-    'Action not delete',
+    "date",
+    "num req",
+    "num hit",
+    "num added",
+    "num deleted",
+    "num redirected",
+    "size redirected",
+    "cache size",
+    "num miss after delete",
+    "size",
+    "capacity",
+    "bandwidth",
+    "bandwidth usage",
+    "hit rate",
+    "weighted hit rate",
+    "written data",
+    "read data",
+    "read on hit data",
+    "read on miss data",
+    "deleted data",
+    "avg free space",
+    "std dev free space",
+    "CPU efficiency",
+    "CPU hit efficiency",
+    "CPU miss efficiency",
+    "CPU efficiency upper bound",
+    "CPU efficiency lower bound",
+    "Addition epsilon",
+    "Eviction epsilon",
+    "Addition qvalue function",
+    "Eviction qvalue function",
+    "Eviction calls",
+    "Eviction forced calls",
+    "Eviction mean num categories",
+    "Eviction std dev num categories",
+    "Action store",
+    "Action not store",
+    "Action delete all",
+    "Action delete half",
+    "Action delete quarter",
+    "Action delete one",
+    "Action not delete",
 ]
 
 
 class Element(object):
-
-    def __init__(self, components: list, filename: str, df: 'pd.DataFrame'):
+    def __init__(self, components: list, filename: str, df: "pd.DataFrame"):
         self._df = df
         self._filename = filename
-        self._components = set([
-            elm for elm in self.__parse_components(components)
-        ])
+        self._components = set([elm for elm in self.__parse_components(components)])
 
     @staticmethod
     def __parse_components(components: list) -> list:
         for component in components:
             if component.find("weightFunLRU") != -1:
                 yield component.rsplit("_", 3)[0]
-            elif component.find("_") != -1 and \
-                    component.split("_")[0] in _ALGORITHMS:
+            elif component.find("_") != -1 and component.split("_")[0] in _ALGORITHMS:
                 yield component.split("_")[0]
             elif component.find("aiRL") != -1:
                 cur_type = component.split("aiRL_")[1].split("_", 1)[0]
@@ -103,7 +100,6 @@ class Element(object):
 
 
 class Results(object):
-
     def __init__(self):
         self._elemets = {}
         self._choices = {}
@@ -111,44 +107,66 @@ class Results(object):
     def __len__(self) -> int:
         return len(self._elemets)
 
-    def insert(self, path: 'pathlib.Path', components: list, filename: str,
-               df: 'pd.DataFrame', choices: 'pd.DataFrame' = None) -> 'Results':
-        elm = Element(components, filename,  df)
+    def insert(
+        self,
+        path: "pathlib.Path",
+        components: list,
+        filename: str,
+        df: "pd.DataFrame",
+        choices: "pd.DataFrame" = None,
+    ) -> "Results":
+        elm = Element(components, filename, df)
         self._elemets[path.as_posix()] = elm
         self._choices[path.as_posix()] = choices
         return self
 
-    @ property
+    @property
     def components(self) -> set:
         components = set()
         for elm in self._elemets.values():
             components |= elm.components
         return sorted(components)
 
-    @ property
-    def files(self) -> 'list[str]':
+    @property
+    def files(self) -> "list[str]":
         return list(sorted(self._elemets.keys()))
 
     def get_all(self):
         for file_, df in self._elemets.items():
             yield file_, df, self.get_log(file_, [], [])
 
-    def get_df(self, file_: str, filters_all: list, filters_any: list) -> 'pd.DataFrame':
+    def get_df(
+        self, file_: str, filters_all: list, filters_any: list
+    ) -> "pd.DataFrame":
         cur_elm = self._elemets[file_]
-        all_ = len(cur_elm.components.intersection(set(filters_all))) == len(
-            filters_all) if len(filters_all) > 0 else True
-        any_ = len(cur_elm.components.intersection(set(filters_any))
-                   ) != 0 if len(filters_any) > 0 else True
+        all_ = (
+            len(cur_elm.components.intersection(set(filters_all))) == len(filters_all)
+            if len(filters_all) > 0
+            else True
+        )
+        any_ = (
+            len(cur_elm.components.intersection(set(filters_any))) != 0
+            if len(filters_any) > 0
+            else True
+        )
         if all_ and any_:
             return cur_elm.df
         return None
 
-    def get_log(self, file_: str, filters_all: list, filters_any: list) -> 'pd.DataFrame':
+    def get_log(
+        self, file_: str, filters_all: list, filters_any: list
+    ) -> "pd.DataFrame":
         cur_elm = self._elemets[file_]
-        all_ = len(cur_elm.components.intersection(set(filters_all))) == len(
-            filters_all) if len(filters_all) > 0 else True
-        any_ = len(cur_elm.components.intersection(set(filters_any))
-                   ) != 0 if len(filters_any) > 0 else True
+        all_ = (
+            len(cur_elm.components.intersection(set(filters_all))) == len(filters_all)
+            if len(filters_all) > 0
+            else True
+        )
+        any_ = (
+            len(cur_elm.components.intersection(set(filters_any))) != 0
+            if len(filters_any) > 0
+            else True
+        )
         if all_ and any_:
             tmp = self._choices[file_]
             if isinstance(tmp, pathlib.Path):
@@ -159,24 +177,22 @@ class Results(object):
             return tmp
 
 
-def aggregate_results(folder: str) -> 'Results':
+def aggregate_results(folder: str) -> "Results":
     abs_target_folder = pathlib.Path(folder).resolve()
     results = Results()
     all_columns = set(COLUMNS)
-    for result_path in tqdm(list(
-        abs_target_folder.glob(f"**/{SIM_RESULT_FILENAME}")
-    ), desc="Opening results"):
+    for result_path in tqdm(
+        list(abs_target_folder.glob(f"**/{SIM_RESULT_FILENAME}")),
+        desc="Opening results",
+    ):
         df = pd.read_csv(result_path)
         cur_columns = set(df.columns)
         if cur_columns.issubset(all_columns):
-            df['date'] = pd.to_datetime(
-                df['date'].apply(lambda elm: elm.split()[0]),
-                format="%Y-%m-%d"
+            df["date"] = pd.to_datetime(
+                df["date"].apply(lambda elm: elm.split()[0]), format="%Y-%m-%d"
             )
-            relative_path = result_path.relative_to(
-                abs_target_folder
-            )
-            * components, filename = relative_path.parts
+            relative_path = result_path.relative_to(abs_target_folder)
+            *components, filename = relative_path.parts
             # Check choices
             choice_file = result_path.parent.joinpath(SIM_CHOICE_LOG_FILE)
             if choice_file.exists():
@@ -187,76 +203,72 @@ def aggregate_results(folder: str) -> 'Results':
     return results
 
 
-def measure_throughput_ratio(df: 'pd.DataFrame') -> 'pd.Series':
-    cache_size = df['cache size'][0]
-    return (df['read on hit data'] - df['written data'])/cache_size
+def measure_throughput_ratio(df: "pd.DataFrame") -> "pd.Series":
+    cache_size = df["cache size"][0]
+    return (df["read on hit data"] - df["written data"]) / cache_size
 
 
-def measure_cost_ratio(df: 'pd.DataFrame') -> 'pd.Series':
-    cache_size = df['cache size'][0]
-    return (df['written data'] + df['deleted data'])/cache_size
+def measure_cost_ratio(df: "pd.DataFrame") -> "pd.Series":
+    cache_size = df["cache size"][0]
+    return (df["written data"] + df["deleted data"]) / cache_size
 
 
-def measure_throughput(df: 'pd.DataFrame') -> 'pd.Series':
+def measure_throughput(df: "pd.DataFrame") -> "pd.Series":
     # to Terabytes
-    return (df['read on hit data'] - df['written data'])/(1024.**2.)
+    return (df["read on hit data"] - df["written data"]) / (1024.0 ** 2.0)
 
 
-def measure_cost(df: 'pd.DataFrame') -> 'pd.Series':
+def measure_cost(df: "pd.DataFrame") -> "pd.Series":
     # to Terabytes
-    return (df['written data'] + df['deleted data'])/(1024.**2.)
+    return (df["written data"] + df["deleted data"]) / (1024.0 ** 2.0)
 
 
-def measure_read_on_hit_ratio(df: 'pd.DataFrame') -> 'pd.Series':
-    return (df['read on hit data']/df['read data']) * 100.
+def measure_read_on_hit_ratio(df: "pd.DataFrame") -> "pd.Series":
+    return (df["read on hit data"] / df["read data"]) * 100.0
 
 
-def measure_cpu_eff(df: 'pd.DataFrame') -> 'pd.Series':
-    return df['CPU efficiency']
+def measure_cpu_eff(df: "pd.DataFrame") -> "pd.Series":
+    return df["CPU efficiency"]
 
 
-def measure_avg_free_space(df: 'pd.DataFrame') -> 'pd.Series':
-    cache_size = df['cache size'][0]
-    return (df['avg free space'] / cache_size) * 100.
+def measure_avg_free_space(df: "pd.DataFrame") -> "pd.Series":
+    cache_size = df["cache size"][0]
+    return (df["avg free space"] / cache_size) * 100.0
 
 
-def measure_std_dev_free_space(df: 'pd.DataFrame') -> 'pd.Series':
-    cache_size = df['cache size'][0]
-    return (df['std dev free space'] / cache_size) * 100.
+def measure_std_dev_free_space(df: "pd.DataFrame") -> "pd.Series":
+    cache_size = df["cache size"][0]
+    return (df["std dev free space"] / cache_size) * 100.0
 
 
-def measure_bandwidth(df: 'pd.DataFrame') -> 'pd.Series':
-    return (df['read on miss data'] / df['bandwidth']) * 100.
+def measure_bandwidth(df: "pd.DataFrame") -> "pd.Series":
+    return (df["read on miss data"] / df["bandwidth"]) * 100.0
 
 
-def measure_redirect_volume(df: 'pd.DataFrame') -> 'pd.Series':
-    cache_size = df['cache size'][0]
-    return (df['size redirected'] / cache_size) * 100.
+def measure_redirect_volume(df: "pd.DataFrame") -> "pd.Series":
+    cache_size = df["cache size"][0]
+    return (df["size redirected"] / cache_size) * 100.0
 
 
-def measure_num_miss_after_delete(df: 'pd.DataFrame') -> 'pd.Series':
-    return df['num miss after delete']
+def measure_num_miss_after_delete(df: "pd.DataFrame") -> "pd.Series":
+    return df["num miss after delete"]
 
 
-def measure_hit_rate(df: 'pd.DataFrame') -> 'pd.Series':
-    return df['hit rate']
+def measure_hit_rate(df: "pd.DataFrame") -> "pd.Series":
+    return df["hit rate"]
 
 
-def measure_hit_over_miss(df: 'pd.DataFrame') -> 'pd.Series':
-    return df['read on hit data'] / df['read on miss data']
+def measure_hit_over_miss(df: "pd.DataFrame") -> "pd.Series":
+    return df["read on hit data"] / df["read on miss data"]
 
 
-def parse_simulation_report(files2plot: list, prefix: str, generator: bool = False, target: str = "AFTERDELETE") -> dict:
+def parse_simulation_report(
+    files2plot: list, prefix: str, generator: bool = False, target: str = "AFTERDELETE"
+) -> dict:
     del_evaluators = {}
 
     for file_, _, sim_log in tqdm(files2plot, desc="Parse log", position=1):
-        name = file_.replace(
-            prefix,
-            ""
-        ).replace(
-            f"/{SIM_RESULT_FILENAME}",
-            ""
-        )
+        name = file_.replace(prefix, "").replace(f"/{SIM_RESULT_FILENAME}", "")
 
         curEvents = []
 
@@ -272,7 +284,9 @@ def parse_simulation_report(files2plot: list, prefix: str, generator: bool = Fal
     return del_evaluators
 
 
-def make_table(files2plot: list, prefix: str) -> 'pd.DataFrame':
+def make_table(
+    files2plot: list, prefix: str, top_n: int = 0, extended: bool = False
+) -> Tuple["pd.DataFrame", list]:
     """Make html table from files to plot
 
     :param files2plot: list of files to plot with their dataframes
@@ -284,10 +298,8 @@ def make_table(files2plot: list, prefix: str) -> 'pd.DataFrame':
     """
     table = []
     for file_, df in files2plot:
-        values = get_measures(file_, df)
-        values[0] = values[0].replace(
-            prefix, "").replace(
-            f"/{SIM_RESULT_FILENAME}", "")
+        values = get_measures(file_, df, extended=extended)
+        values[0] = values[0].replace(prefix, "").replace(f"/{SIM_RESULT_FILENAME}", "")
         search_size = re.search("[\/]?[0-9]*T\/", values[0])
         values[0] = values[0].replace("run_full_normal/", "")
         if search_size != None:
@@ -296,93 +308,97 @@ def make_table(files2plot: list, prefix: str) -> 'pd.DataFrame':
                 "",
                 # f"{search_size.group().replace('/', '')} - "
             )
+        values.insert(0, file_)
         table.append(values)
-    df = pd.DataFrame(
-        table,
-        columns=[
-            "file", "Throughput ratio", "Cost ratio",
-            "Throughput (TB)", "Cost (TB)",
-            "Read on hit ratio", "Bandwidth",
-            "Redirect Vol.", "Avg. Free Space",
-            "Std. Dev. Free Space", "Hit over Miss",
-            "Num. miss after del.", "Hit rate", "CPU Eff."
+    if extended:
+        columns = [
+            "source",
+            "file",
+            "Throughput ratio",
+            "Cost ratio",
+            "Throughput (TB)",
+            "Cost (TB)",
+            "Read on hit ratio",
+            "Bandwidth",
+            "Redirect Vol.",
+            "Avg. Free Space",
+            "Std. Dev. Free Space",
+            "Hit over Miss",
+            "Num. miss after del.",
+            "Hit rate",
+            "CPU Eff.",
         ]
-    )
+    else:
+        columns = [
+            "source",
+            "file",
+            "Throughput ratio",
+            "Cost ratio",
+            "Bandwidth",
+            "Num. miss after del.",
+            "Hit rate",
+            "CPU Eff.",
+        ]
+    df = pd.DataFrame(table, columns=columns)
     df = df.sort_values(
         by=["Throughput ratio", "Cost ratio", "Num. miss after del."],
         ascending=[False, True, False],
     )
-    df = df.round(3)
-    return df
+    df = df.round(6)
+    if top_n != 0:
+        df = df.iloc[:10]
+
+    source_files = df.source.to_list()
+    del df["source"]
+
+    return df, source_files
 
 
-def get_measures(cache_filename: str, df: 'pd.DataFrame') -> list:
+def get_measures(
+    cache_filename: str, df: "pd.DataFrame", extended: bool = False
+) -> list:
     measures = [cache_filename]
     # print(cache_filename)
 
     # Throughput ratio
-    measures.append(
-        measure_throughput_ratio(df).mean()
-    )
+    measures.append(measure_throughput_ratio(df).mean())
 
     # Cost ratio
-    measures.append(
-        measure_cost_ratio(df).mean()
-    )
+    measures.append(measure_cost_ratio(df).mean())
 
-    # Throughput (TB)
-    measures.append(
-        measure_throughput(df).mean()
-    )
+    if extended:
+        # Throughput (TB)
+        measures.append(measure_throughput(df).mean())
 
-    # Cost (TB)
-    measures.append(
-        measure_cost(df).mean()
-    )
+        # Cost (TB)
+        measures.append(measure_cost(df).mean())
 
-    # Read on hit ratio
-    measures.append(
-        measure_read_on_hit_ratio(df).mean()
-    )
+        # Read on hit ratio
+        measures.append(measure_read_on_hit_ratio(df).mean())
 
     # Bandwidth
-    measures.append(
-        measure_bandwidth(df).mean()
-    )
+    measures.append(measure_bandwidth(df).mean())
 
-    # Redirect Vol.
-    measures.append(
-        measure_redirect_volume(df).mean()
-    )
+    if extended:
+        # Redirect Vol.
+        measures.append(measure_redirect_volume(df).mean())
 
-    # Avg. Free Space
-    measures.append(
-        measure_avg_free_space(df).mean()
-    )
+        # Avg. Free Space
+        measures.append(measure_avg_free_space(df).mean())
 
-    # Std. Dev. Free Space
-    measures.append(
-        measure_std_dev_free_space(df).mean()
-    )
+        # Std. Dev. Free Space
+        measures.append(measure_std_dev_free_space(df).mean())
 
-    # Hit over Miss
-    measures.append(
-        measure_hit_over_miss(df).mean()
-    )
+        # Hit over Miss
+        measures.append(measure_hit_over_miss(df).mean())
 
     # Num. miss after delete
-    measures.append(
-        measure_num_miss_after_delete(df).mean()
-    )
+    measures.append(measure_num_miss_after_delete(df).mean())
 
     # Hit rate
-    measures.append(
-        measure_hit_rate(df).mean()
-    )
+    measures.append(measure_hit_rate(df).mean())
 
     # CPU Efficiency
-    measures.append(
-        measure_cpu_eff(df).mean()
-    )
+    measures.append(measure_cpu_eff(df).mean())
 
     return measures
