e8cf163aff52bd6dd76b67b794efe1d7eedc1b08
MircoT <MircoT@users.noreply.github.com>
2020-12-07 12:35:47 +0100

    Fis simulator utils main

--- SmartCache/sim/utils/simulator.py
+++ SmartCache/sim/utils/simulator.py
@@ -1,13 +1,9 @@
 import logging
 import subprocess
-import sys
 from os import path
-import argparse
 import pathlib
 
 import coloredlogs
-
-from .utils import str2bool
 
 SIM_NAME = "simulator"
 
@@ -56,29 +52,3 @@ def get_simulator_exe(force_creation: bool = False, release: bool = False, fast:
             logger.info(f"[BUILD]->[ERROR]{err.output.decode('utf-8')}")
             exit(-1)
     return sim_path.as_posix()
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Go Simulator Utils')
-
-    parser.register('type', 'bool', str2bool)  # add type keyword to registries
-
-    parser.add_argument(
-        'command', type=str, choices=["compile", "simPath"],
-        help='the command to execute'
-    )
-    parser.add_argument(
-        '--release', type='bool', default=False,
-        help='build the release binary'
-    )
-    parser.add_argument(
-        '--fast', type='bool', default=False,
-        help='build only modified files'
-    )
-
-    args = parser.parse_args()
-    if args.command == "compile":
-        get_simulator_exe(True, args.release, args.fast)
-    elif args.command == "simPath":
-        _, simPath = get_simulator_path()
-        print(simPath.as_posix())
