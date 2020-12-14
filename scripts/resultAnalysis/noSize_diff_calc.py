159c1f9874dc95cc9eb36ba5fbb55211f2e66921
MircoT <MircoT@users.noreply.github.com>
2020-12-14 21:50:31 +0100

    Update scripts

--- scripts/resultAnalysis/noSize_diff_calc.py
+++ scripts/resultAnalysis/noSize_diff_calc.py
@@ -17,9 +17,9 @@ for row in df.itertuples():
     name = row.file
     size = None
     epsilon = None
-    if name.find("epsilon") != -1:
-        epsilon, name = name.split("] ")
-        epsilon = epsilon.replace("[epsilon", "").strip()
+    if name.find("feature") != -1:
+        epsilon, name = name.split("feature] ")
+        epsilon = epsilon.replace("[NO", "").replace("_", "").strip()
     name = name.replace("aiRL_", "")
     method, size, *_ = name.split("_")
 
@@ -35,7 +35,7 @@ for row in df.itertuples():
             results[size] = {}
 
         if method not in results[size]:
-            results[size][method] = {"fast": {}, "slow": {}, "normal": {}}
+            results[size][method] = {"size": {}, "normal": {}}
 
         if epsilon is None:
             epsilon = "normal"
@@ -49,14 +49,10 @@ for row in df.itertuples():
 
         # print(results[size][name][epsilon])
 
-fast_throughput_diffs = []
-fast_cost_diffs = []
-fast_readOnHitRatio_diffs = []
-fast_bandwidth_diffs = []
-slow_throughput_diffs = []
-slow_cost_diffs = []
-slow_readOnHitRatio_diffs = []
-slow_bandwidth_diffs = []
+no_size_throughput_diffs = []
+no_size_cost_diffs = []
+no_size_readOnHitRatio_diffs = []
+no_size_bandwidth_diffs = []
 
 calc_diff_perc = lambda a, b: ((abs(a - b) / ((a + b) / 2.0)) * 100.0) * (
     -1.0 if a > b else 1.0
@@ -64,30 +60,18 @@ calc_diff_perc = lambda a, b: ((abs(a - b) / ((a + b) / 2.0)) * 100.0) * (
 
 for size, methods in results.items():
     for method, values in methods.items():
-        if values["slow"] and values["fast"]:
-            throughput_diff_slow = calc_diff_perc(
-                values["normal"]["throughput"], values["slow"]["throughput"]
+        if values["size"]:
+            throughput_diff_no_size = calc_diff_perc(
+                values["normal"]["throughput"], values["size"]["throughput"]
             )
-            throughput_diff_fast = calc_diff_perc(
-                values["normal"]["throughput"], values["fast"]["throughput"]
+            cost_diff_no_size = calc_diff_perc(
+                values["normal"]["cost"], values["size"]["cost"]
             )
-            cost_diff_slow = calc_diff_perc(
-                values["normal"]["cost"], values["slow"]["cost"]
+            readOnHitRatio_diff_no_size = calc_diff_perc(
+                values["normal"]["readOnHitRatio"], values["size"]["readOnHitRatio"]
             )
-            cost_diff_fast = calc_diff_perc(
-                values["normal"]["cost"], values["fast"]["cost"]
-            )
-            readOnHitRatio_diff_slow = calc_diff_perc(
-                values["normal"]["readOnHitRatio"], values["slow"]["readOnHitRatio"]
-            )
-            readOnHitRatio_diff_fast = calc_diff_perc(
-                values["normal"]["readOnHitRatio"], values["fast"]["readOnHitRatio"]
-            )
-            bandwidth_diff_slow = calc_diff_perc(
-                values["normal"]["bandwidth"], values["slow"]["bandwidth"]
-            )
-            bandwidth_diff_fast = calc_diff_perc(
-                values["normal"]["bandwidth"], values["fast"]["bandwidth"]
+            bandwidth_diff_no_size = calc_diff_perc(
+                values["normal"]["bandwidth"], values["size"]["bandwidth"]
             )
 
             # print(values)
@@ -95,63 +79,43 @@ for size, methods in results.items():
                 f"|{size}",
                 method,
                 "throughput diff\n|-> ",
-                "fast:",
-                throughput_diff_fast,
-                "\tslow:",
-                throughput_diff_slow,
+                "no size:",
+                throughput_diff_no_size,
             )
             print(
                 f"|{size}",
                 method,
                 "cost diff\n|-> ",
-                "fast:",
-                cost_diff_fast,
-                "\tslow:",
-                cost_diff_slow,
+                "no size:",
+                cost_diff_no_size,
             )
             print(
                 f"|{size}",
                 method,
                 "readOnHitRatio diff\n|-> ",
-                "fast:",
-                readOnHitRatio_diff_fast,
-                "\tslow:",
-                readOnHitRatio_diff_slow,
+                "no size:",
+                readOnHitRatio_diff_no_size,
             )
             print(
                 f"|{size}",
                 method,
                 "bandwidth diff\n|-> ",
-                "fast:",
-                bandwidth_diff_fast,
-                "\tslow:",
-                bandwidth_diff_slow,
+                "no size:",
+                bandwidth_diff_no_size,
             )
 
-            fast_throughput_diffs.append(throughput_diff_fast)
-            fast_cost_diffs.append(cost_diff_fast)
-            fast_readOnHitRatio_diffs.append(readOnHitRatio_diff_fast)
-            fast_bandwidth_diffs.append(bandwidth_diff_fast)
-            slow_throughput_diffs.append(throughput_diff_slow)
-            slow_cost_diffs.append(cost_diff_slow)
-            slow_readOnHitRatio_diffs.append(readOnHitRatio_diff_slow)
-            slow_bandwidth_diffs.append(bandwidth_diff_slow)
+            no_size_throughput_diffs.append(throughput_diff_no_size)
+            no_size_cost_diffs.append(cost_diff_no_size)
+            no_size_readOnHitRatio_diffs.append(readOnHitRatio_diff_no_size)
+            no_size_bandwidth_diffs.append(bandwidth_diff_no_size)
 
-fast_throughput_diffs = np.array(fast_throughput_diffs)
-fast_cost_diffs = np.array(fast_cost_diffs)
-fast_readOnHitRatio_diffs = np.array(fast_readOnHitRatio_diffs)
-fast_bandwidth_diffs = np.array(fast_bandwidth_diffs)
-slow_throughput_diffs = np.array(slow_throughput_diffs)
-slow_cost_diffs = np.array(slow_cost_diffs)
-slow_readOnHitRatio_diffs = np.array(slow_readOnHitRatio_diffs)
-slow_bandwidth_diffs = np.array(slow_bandwidth_diffs)
+no_size_throughput_diffs = np.array(no_size_throughput_diffs)
+no_size_cost_diffs = np.array(no_size_cost_diffs)
+no_size_readOnHitRatio_diffs = np.array(no_size_readOnHitRatio_diffs)
+no_size_bandwidth_diffs = np.array(no_size_bandwidth_diffs)
 
 print("---")
-print("fast throughput mean diff %", np.mean(fast_throughput_diffs))
-print("fast cost mean diff %", np.mean(fast_cost_diffs))
-print("fast readOnHitRatio mean diff %", np.mean(fast_readOnHitRatio_diffs))
-print("fast bandwidth mean diff %", np.mean(fast_bandwidth_diffs))
-print("slow throughput mean diff %", np.mean(slow_throughput_diffs))
-print("slow cost mean diff %", np.mean(slow_cost_diffs))
-print("slow readOnHitRatio mean diff %", np.mean(slow_readOnHitRatio_diffs))
-print("slow bandwidth mean diff %", np.mean(slow_bandwidth_diffs))
+print("no size throughput mean diff %", np.mean(no_size_throughput_diffs))
+print("no size cost mean diff %", np.mean(no_size_cost_diffs))
+print("no size readOnHitRatio mean diff %", np.mean(no_size_readOnHitRatio_diffs))
+print("no size bandwidth mean diff %", np.mean(no_size_bandwidth_diffs))
