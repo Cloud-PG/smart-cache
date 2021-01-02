a20313facca416af6f4a69a6d0c6918c0d557fb7
MircoT <MircoT@users.noreply.github.com>
2021-01-02 16:37:44 +0100

    Update build git hash

--- SmartCache/sim/build_sim.sh
+++ SmartCache/sim/build_sim.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo "[BUILD SIMULATOR]" &&
-go build -a -v -o bin -ldflags "-X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash=`git rev-parse HEAD`" ./... &&
-echo "[BUILD SIMULATOR][DONE]"
+    go build -a -v -o bin -ldflags "-X main.buildstamp=$(date -u '+%Y-%m-%d_%I:%M:%S%p') -X main.githash=$(git rev-parse --short HEAD)" ./... &&
+    echo "[BUILD SIMULATOR][DONE]"
--- SmartCache/sim/build_sim_release.sh
+++ SmartCache/sim/build_sim_release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "[BUILD SIMULATOR]" &&
-go build -a -v -o bin -ldflags "-s -w -X main.buildstamp=`date -u '+%Y-%m-%d_%I:%M:%S%p'` -X main.githash=`git rev-parse HEAD`" ./... &&
-# upx --brute bin/simulator # to cut executable size by compression
-echo "[BUILD SIMULATOR][DONE]"
+    go build -a -v -o bin -ldflags "-s -w -X main.buildstamp=$(date -u '+%Y-%m-%d_%I:%M:%S%p') -X main.githash=$(git rev-parse --short HEAD)" ./... &&
+    # upx --brute bin/simulator # to cut executable size by compression
+    echo "[BUILD SIMULATOR][DONE]"
