b7e9ad3b2ac0d37095f44e57ff588d331479cf70
MircoT <MircoT@users.noreply.github.com>
2021-02-16 14:06:35 +0100

    fix prepare window

--- scripts/tests/prepare_windows.sh
+++ scripts/tests/prepare_windows.sh
@@ -9,70 +9,70 @@ pushd $BASE_DIR
 mkdir -p {jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec}
 
 echo "Prepare period window jan"
-find . -name "results*_2018-01-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./jan/)
+find . -name "results*_2018-01-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./jan/cur_file
 echo "Prepare period window feb"
-find . -name "results*_2018-02-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./feb/)
+find . -name "results*_2018-02-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./feb/cur_file
 echo "Prepare period window mar"
-find . -name "results*_2018-03-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./mar/)
+find . -name "results*_2018-03-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./mar/cur_file
 echo "Prepare period window apr"
-find . -name "results*_2018-04-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./apr/)
+find . -name "results*_2018-04-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./apr/cur_file
 echo "Prepare period window may"
-find . -name "results*_2018-05-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./may/)
+find . -name "results*_2018-05-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./may/cur_file
 echo "Prepare period window jun"
-find . -name "results*_2018-06-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./jun/)
+find . -name "results*_2018-06-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./jun/cur_file
 echo "Prepare period window jul"
-find . -name "results*_2018-07-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./jul/)
+find . -name "results*_2018-07-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./jul/cur_file
 echo "Prepare period window aug"
-find . -name "results*_2018-08-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./aug/)
+find . -name "results*_2018-08-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./aug/cur_file
 echo "Prepare period window sep"
-find . -name "results*_2018-09-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./sep/)
+find . -name "results*_2018-09-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./sep/cur_file
 echo "Prepare period window oct"
-find . -name "results*_2018-10-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./oct/)
+find . -name "results*_2018-10-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./oct/cur_file
 echo "Prepare period window nov"
-find . -name "results*_2018-11-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./nov/)
+find . -name "results*_2018-11-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./nov/cur_file
 echo "Prepare period window dec"
-find . -name "results*_2018-12-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./dec/)
+find . -name "results*_2018-12-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./dec/cur_file
 
 mkdir -p {jan-mar,apr-jun,jul-sep,oct-dec}
 
 echo "Prepare period window jan-mar"
-find . -name "results*_2018-01-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./jan-mar/)
-find . -name "results*_2018-02-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./jan-mar/)
-find . -name "results*_2018-03-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./jan-mar/)
+find . -name "results*_2018-01-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./jan-mar/cur_file
+find . -name "results*_2018-02-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./jan-mar/cur_file
+find . -name "results*_2018-03-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./jan-mar/cur_file
 
 echo "Prepare period window apr-jun"
-find . -name "results*_2018-04-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./apr-jun/)
-find . -name "results*_2018-05-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./apr-jun/)
-find . -name "results*_2018-06-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./apr-jun/)
+find . -name "results*_2018-04-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./apr-jun/cur_file
+find . -name "results*_2018-05-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./apr-jun/cur_file
+find . -name "results*_2018-06-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./apr-jun/cur_file
 
 echo "Prepare period window jul-sep"
-find . -name "results*_2018-07-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./jul-sep/)
-find . -name "results*_2018-08-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./jul-sep/)
-find . -name "results*_2018-09-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./jul-sep/)
+find . -name "results*_2018-07-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./jul-sep/cur_file
+find . -name "results*_2018-08-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./jul-sep/cur_file
+find . -name "results*_2018-09-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./jul-sep/cur_file
 
 echo "Prepare period window oct-dec"
-find . -name "results*_2018-10-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./oct-dec/)
-find . -name "results*_2018-11-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./oct-dec/)
-find . -name "results*_2018-12-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./oct-dec/)
+find . -name "results*_2018-10-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./oct-dec/cur_file
+find . -name "results*_2018-11-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./oct-dec/cur_file
+find . -name "results*_2018-12-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./oct-dec/cur_file
 
 mkdir -p {jan-apr,may-aug,sep-dec}
 
 echo "Prepare period window jan-apr"
-find . -name "results*_2018-01-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./jan-apr/)
-find . -name "results*_2018-02-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./jan-apr/)
-find . -name "results*_2018-03-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./jan-apr/)
-find . -name "results*_2018-04-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./jan-apr/)
+find . -name "results*_2018-01-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./jan-apr/cur_file
+find . -name "results*_2018-02-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./jan-apr/cur_file
+find . -name "results*_2018-03-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./jan-apr/cur_file
+find . -name "results*_2018-04-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./jan-apr/cur_file
 
 echo "Prepare period window may-aug"
-find . -name "results*_2018-05-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./may-aug/)
-find . -name "results*_2018-06-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./may-aug/)
-find . -name "results*_2018-07-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./may-aug/)
-find . -name "results*_2018-08-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./may-aug/)
+find . -name "results*_2018-05-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./may-aug/cur_file
+find . -name "results*_2018-06-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./may-aug/cur_file
+find . -name "results*_2018-07-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./may-aug/cur_file
+find . -name "results*_2018-08-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./may-aug/cur_file
 
 echo "Prepare period window sep-dec"
-find . -name "results*_2018-09-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./sep-dec/)
-find . -name "results*_2018-10-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./sep-dec/)
-find . -name "results*_2018-11-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./sep-dec/)
-find . -name "results*_2018-12-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) $(realpath ./sep-dec/)
+find . -name "results*_2018-09-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./sep-dec/cur_file
+find . -name "results*_2018-10-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./sep-dec/cur_file
+find . -name "results*_2018-11-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./sep-dec/cur_file
+find . -name "results*_2018-12-*.csv*" | xargs -I cur_file ln -sf $(realpath cur_file) ./sep-dec/cur_file
 
 popd
